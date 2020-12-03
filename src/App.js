6ff5cd2cf5878d99dfe1c76b0cd9942614730120
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2020-12-03 10:12:45 -0500

    Imported react-router-dom classes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Dashboard from "./components/Dashboard";
 import Login from "./components/Login";
 
