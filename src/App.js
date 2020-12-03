4781c17921d8aac8f042bf9de7d661cc19c91b9b
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2020-12-03 09:28:55 -0500

    Added DashboardJSX element to App return statement

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
+import Dashboard from "./components/Dashboard";
 import Login from "./components/Login";
+
 
 const App = () => {
   return (
     <div>
       <Login />
+      <Dashboard />
     </div>
   );
 }
