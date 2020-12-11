3b672600d8a4ab3f0744bf545e6b6e3ee18e937b
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2020-12-11 15:32:39 -0500

    Added div wrapper in JSX

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,7 +2,9 @@ import React from "react";
 
 function Dashboard () {
     return(
-        <h1>This is the dashboard</h1>
+        <div>
+            <h1>This is the dashboard</h1>       
+        </div>
     );
 };
 
