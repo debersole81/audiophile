2ac06bc92c04a06c2c65d87fd2acb64bb9185ca5
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2020-12-11 16:38:16 -0500

    Created logout button

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -4,6 +4,9 @@ function Dashboard () {
     return(
         <div>
             <h1>This is the dashboard</h1>       
+            <button>
+                Logout
+            </button>
         </div>
     );
 };
