358f8e0eb2eb16e2b461c0aa70fbdb3813195bb4
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2020-12-14 16:58:42 -0500

    Created Component and isAuthenticated variables

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -1,7 +1,12 @@
 import React from "react";
+import { Redirect } from "react-router-dom";
 
-function ProtectedRoute () {
+function ProtectedRoute (Component) {
+    const Component = props.Component;
+    const isAuthenticated = 
+    
     return(
+         
         <h1>This is a protected route</h1>
     );
 };
