e1d9073c341e3acef5f9e2af62399ae36e7543b8
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2020-12-22 11:27:03 -0500

    Added history.push method to user authentication conditional in loginSubmit handler

--- src/App.js
+++ src/App.js
@@ -41,61 +41,56 @@ function App () {
   const loginSubmit = (e) => { //runs when submit button on login component is clicked.
     e.preventDefault(); //prevents login component from re-rendering on click
 
-    history.push("/dashboard");
+    // history.push("/dashboard");
 
-    // //username and password variables and arrays
-    // const loginUsername = loginInput.username; //retains the username input value from the login form
-    // const loginPassword = loginInput.password; //retains the password input value from the login form 
-    // const userUsername = userDatabase.map(username => username.username); //maps through userDatabase and returns an array containing the username property value in each element
-    // const userNameMatchValue = []; //empty array that will store the truthy value from the nested coniditonal statement in the userUsername.forEach method
+    //username and password variables and arrays
+    const loginUsername = loginInput.username; //retains the username input value from the login form
+    const loginPassword = loginInput.password; //retains the password input value from the login form 
+    const userUsername = userDatabase.map(username => username.username); //maps through userDatabase and returns an array containing the username property value in each element
+    const userNameMatchValue = []; //empty array that will store the truthy value from the nested coniditonal statement in the userUsername.forEach method
 
 
 
-    // //username and password validation
-    // let usernameValidation = false; //username validation is false by default
-    // let passwordValidation = false; //password validation is false by default
+    //username and password validation
+    let usernameValidation = false; //username validation is false by default
+    let passwordValidation = false; //password validation is false by default
     
-    // userUsername.forEach((value) => { //compares username input value from login form to each username in userDatabase. if equal, sets usernameValidation as true and pushes input value to a new array
-    //   if(value == loginUsername) {
-    //     usernameValidation = true;
-    //     return(userNameMatchValue.push(value)); 
-    //   };
-    // });
+    userUsername.forEach((value) => { //compares username input value from login form to each username in userDatabase. if equal, sets usernameValidation as true and pushes input value to a new array
+      if(value == loginUsername) {
+        usernameValidation = true;
+        return(userNameMatchValue.push(value)); 
+      };
+    });
     
-    // const userNameValueString = userNameMatchValue.toString(); //converts the userNameMatchValue array to a string and assigns it to a new variable, userNameValueString
+    const userNameValueString = userNameMatchValue.toString(); //converts the userNameMatchValue array to a string and assigns it to a new variable, userNameValueString
 
-    // let userId = userDatabase.find(item => item.username === userNameValueString); //searches userDatabase and returns an array object with a username attribute that matches the userNameValueString value. returns undefined if no match.
+    let userId = userDatabase.find(item => item.username === userNameValueString); //searches userDatabase and returns an array object with a username attribute that matches the userNameValueString value. returns undefined if no match.
     
-    // if(userId === undefined) { //if userDatabase.find returns an undefined value for userId, sets userId to null. 
-    //   userId = null;
-    // } else {
-    //   userId = userId.id; //if userDatabase.find returns a matching object, sets userId equal to the object's id property.
-    // };
+    if(userId === undefined) { //if userDatabase.find returns an undefined value for userId, sets userId to null. 
+      userId = null;
+    } else {
+      userId = userId.id; //if userDatabase.find returns a matching object, sets userId equal to the object's id property.
+    };
 
-    // let userPassword = userDatabase.find(item => item.id === userId); //searches userDatabase and returns an array object with an id attribute that matches the userId value. returns undefined if no match.
+    let userPassword = userDatabase.find(item => item.id === userId); //searches userDatabase and returns an array object with an id attribute that matches the userId value. returns undefined if no match.
    
-    // if(userPassword === undefined) { //if userDatabase.find returns an undefined value for userPassword, sets userPassword to null.
-    //   userPassword = null;
-    // } else {
-    //   userPassword = userPassword.password; //if userDatabase.find returns a matching object, sets userPassword equal to the object's password property.
-    // };
+    if(userPassword === undefined) { //if userDatabase.find returns an undefined value for userPassword, sets userPassword to null.
+      userPassword = null;
+    } else {
+      userPassword = userPassword.password; //if userDatabase.find returns a matching object, sets userPassword equal to the object's password property.
+    };
 
-    // if(userPassword == loginPassword) { //compares password input value from login form to userPassword value. if equal, sets passwordValidation to true.
-    //   passwordValidation = true;
-    // };
+    if(userPassword == loginPassword) { //compares password input value from login form to userPassword value. if equal, sets passwordValidation to true.
+      passwordValidation = true;
+    };
 
 
 
-    // //setting user authentication
-    // if(usernameValidation && passwordValidation === true) { //if userValidation and passwordValidation are both true, sets userAuth variable to true.
-    //   setUserAuth(true);
-    // };
-
-    // //pushing to dashboard
-    // if(userAuth === true) {
-    //   history.push("/dashboard");
-    // };
-
+    //setting user authentication
+    if(usernameValidation && passwordValidation === true) { //if userValidation and passwordValidation are both true, sets userAuth variable to true.
+      setUserAuth(true);
+      history.push("/dashboard");      
+    };
   };
 
   
