511f7001f9ef2a29e2be926c2af3deaa31b48e6d
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2020-12-28 18:59:21 -0500

    Initial functional component build

---
+++ src/components/Search.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Search () {
+
+
+    return(
+        <div>
+            <h1>This is the search component</h1>
+            <button>
+                Back
+            </button>
+        </div>
+    );
+};
+
+export default Search;
