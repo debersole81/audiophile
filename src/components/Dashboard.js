9d6a33411781dd83398ccef057f6262ee6a38935
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2020-12-28 18:54:23 -0500

    Built list elements for child components

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -3,16 +3,22 @@ import { useHistory } from "react-router-dom";
 
 function Dashboard (props) {
 
-    //react router hooks    
-    const history = useHistory();
+    // //react router hooks    
+    // const history = useHistory(); //history points to the useHistory hook
 
-    if(props.userAuth === false) { //prevents user from accessing dashboard without authenticating 
-        history.push("/"); //if userAuth is false, redirects to login
-    }
+    // if(props.userAuth === false) { //prevents user from accessing dashboard without authenticating 
+    //     history.push("/"); //if userAuth is false, redirects to login
+    // }
 
     return(
         <div>
             <h1>This is the dashboard</h1>       
+            <ul>
+                <li>Collection</li>
+                <li>Wish List</li>
+                <li>Randomizer</li>
+                <li>Search</li>
+            </ul>
             <button onClick={props.logoutSubmit}>
                 Logout
             </button>
