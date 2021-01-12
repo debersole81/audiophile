7797cc8eb2236a44f7fdd565428ec90b4d4e5f09
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2021-01-12 12:02:57 -0500

    Declared items variable equal to data.json to convert data from fetchItems to .json

--- src/components/Search.js
+++ src/components/Search.js
@@ -9,8 +9,10 @@ function Search () {
     const fetchItems = async () => { //makes an async call to discogs api
         const data = await fetch ( //fetches data from discogs api and assigns it to a data variable
             `https://api.discogs.com/releases/249504`
-        )
-        console.log(data);
+        );
+
+        const items = await data.json(); //converts data from fetchItems to .json
+        console.log(items);
     };
 
     return(
