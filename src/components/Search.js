f78957ab84ed63493147a49dbc5451b74c44212a
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2021-01-12 11:44:37 -0500

    Wrote fetchItems async function to call and fetch data from discogs API

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,6 +2,12 @@ import React, {useState, useEffect} from "react";
 
 function Search () {
 
+    const fetchItems = async () => { //makes an async call to discogs api
+        const data = await fetch ( //fetches data from discogs api and assigns it to a data variable
+            `https://api.discogs.com/releases/249504`
+        )
+        console.log(data);
+    };
 
     return(
         <div>
