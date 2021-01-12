47f08f690be46f3bda3671314108cdb813e97d33
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2021-01-12 11:56:57 -0500

    Declared useEffect hook to run fetchItems function after the component mounts

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,6 +2,10 @@ import React, {useState, useEffect} from "react";
 
 function Search () {
 
+    useEffect(() => { //runs the fetchItems function after the component mounts
+        fetchItems();
+    }, []); //empty brackets ensures that useEffect will only run after the component mounts
+
     const fetchItems = async () => { //makes an async call to discogs api
         const data = await fetch ( //fetches data from discogs api and assigns it to a data variable
             `https://api.discogs.com/releases/249504`
