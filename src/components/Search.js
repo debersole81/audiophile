aa6360eeea9ba33b23b82be79dfa8e4e418b95f4
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2021-01-12 12:07:40 -0500

    Declared artist state manager using useState hook

--- src/components/Search.js
+++ src/components/Search.js
@@ -6,6 +6,8 @@ function Search () {
         fetchItems();
     }, []); //empty brackets ensures that useEffect will only run after the component mounts
 
+    const [artists, setArtists] = useState([]); //artist state manager
+    
     const fetchItems = async () => { //makes an async call to discogs api
         const data = await fetch ( //fetches data from discogs api and assigns it to a data variable
             `https://api.discogs.com/releases/249504`
