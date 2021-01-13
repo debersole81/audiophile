3d8105b9731adc095fd12f91bbadddd4f349a60b
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2021-01-13 14:12:36 -0500

    Added dashboard state manager, change handlers, and conditional components to toggle the rendering of links to child components on the dashboard path

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { useHistory, Link, Route, Switch } from "react-router-dom";
+import React, {useState} from "react";
+import {useHistory, Link, Route, Switch} from "react-router-dom";
 import Search from "./Search";
 import Collection from "./Collection";
 import WishList from "./WishList";
@@ -15,9 +15,42 @@ function Dashboard (props) {
     //     history.push("/"); //if userAuth is false, redirects to login
     // }
 
-    return(
+    const [viewDash, setViewDash] = useState(true); //dashboard state manager
+
+    const handleViewDashTrue = () => { //sets viewDash to true
+        setViewDash(true);
+    };
+
+    const handleViewDashFalse = () => { //sets viewDash to false
+        setViewDash(false);
+    };
+
+    if(viewDash) { //if viewDash is true, render links to each component on dashboard path.
+        return(
+            <div>
+                <Nav onChangeTrue={handleViewDashTrue} onChangeFalse={handleViewDashFalse} />
+                <Link to="/dashboard/collection" onClick={handleViewDashFalse}> Collection </Link> <br />
+                <Link to="/dashboard/wishlist" onClick={handleViewDashFalse}> Wish List </Link> <br />
+                <Link to="/dashboard/randomizer" onClick={handleViewDashFalse}> Randomizer </Link> <br />
+                <Link to="/dashboard/search" onClick={handleViewDashFalse}> Search </Link>
+                <Switch>
+                    <Route exact path="/dashboard/collection" component={Collection} />
+                    <Route exact path="/dashboard/wishlist" component={WishList} />
+                    <Route exact path="/dashboard/randomizer" component={Randomizer} />
+                    <Route exact path="/dashboard/search" component={Search} />
+                </Switch>
+                <br />            
+                <button onClick={props.logoutSubmit}>
+                    Logout
+                </button>
+            </div>
+        );
+    
+    }
+
+    return( //if viewDash is false, remove links to each component on dashboard path
         <div>
-            <Nav />
+            <Nav onChangeTrue={handleViewDashTrue} onChangeFalse={handleViewDashFalse} />
             <Switch>
                 <Route exact path="/dashboard/collection" component={Collection} />
                 <Route exact path="/dashboard/wishlist" component={WishList} />
