857053f3f038d8434b9c13c5bc0a16a9088684a5
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2021-01-13 14:16:26 -0500

    Added change handlers to change viewDash state in Dashboard component when component links are clicked from the Nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,17 +2,27 @@ import React from "react";
 import "../App.css";
 import { Link } from "react-router-dom";
 
-function Nav() {
+function Nav(props) {
+
+    const handleDashClickTrue = () => { //sets viewDash in Dashboard component to true
+        console.log(props);
+        props.onChangeTrue();
+    };
+
+    const handleDashClickFalse = () => { //sets vieDash in Dashboard component to false
+        console.log("setting false");
+        props.onChangeFalse();
+    };
 
     return (
         <nav className="nav">
             <h3>Logo</h3>
             <ul className="nav-links">
-                <Link to="/dashboard"><li>Dashboard</li></Link>
-                <Link to="/dashboard/collection"><li>Collection</li></Link>
-                <Link to="/dashboard/wishlist"><li>Wish List</li></Link>
-                <Link to="/dashboard/randomizer"><li>Randomizer</li></Link>
-                <Link to="/dashboard/search"><li>Search</li></Link>
+                <Link to="/dashboard" onClick={handleDashClickTrue}><li>Dashboard</li></Link>
+                <Link to="/dashboard/collection" onClick={handleDashClickFalse}><li>Collection</li></Link>
+                <Link to="/dashboard/wishlist" onClick={handleDashClickFalse}><li>Wish List</li></Link>
+                <Link to="/dashboard/randomizer" onClick={handleDashClickFalse}><li>Randomizer</li></Link>
+                <Link to="/dashboard/search" onClick={handleDashClickFalse}><li>Search</li></Link>
             </ul>
         </nav>
     );
