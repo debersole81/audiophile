16595c61b8b8486422b8eaf156cf52ba4e98c2ac
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2021-01-27 12:08:06 -0500

    Added useEffect hook and build URL logic

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,11 +1,21 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 function SearchResults () {
 
-    /*State managers*/
+    /**State managers*/
     const [error, setError] = useState(null);
     const [isLoaded, setIsLoaded] = useState(false);
     const [items, setItems] = useState([]);
+
+    useEffect(() => {
+        /**Build URL*/
+        let url = baseURL;
+        for (let [key, value] of Object.entries(query)) {
+            url = url + key + '=' + value + '&';
+        }
+        url = url.slice(0, -1);          
+    }, []);
+
 };
 
 export default SearchResults;
