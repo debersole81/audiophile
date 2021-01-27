934567bf7f54b87652663f85136e87095775d445
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2021-01-27 11:46:49 -0500

    Added state managers for error, isLoaded, and items

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -2,6 +2,10 @@ import React, { useState } from 'react';
 
 function SearchResults () {
 
+    /*State managers*/
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [items, setItems] = useState([]);
 };
 
 export default SearchResults;
