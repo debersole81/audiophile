4d66ef55ffdfb9e96e2d5f44718578fd40369a93
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2021-01-26 19:33:09 -0500

    Deleted incorrect syntax and built http header variable and query object

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,37 +1,29 @@
 import React, { useState, useEffect } from 'react';
 
 
+/*Build HTTP header*/
+const baseURL = 'https://api.discogs.com/database/search?'; //q=Nirvana&key=KNMVnsceTtAqbvAVbsPX&secret=YjfVFNTeaEqVblcDGkanBBRSWPAeIXBO'
+const query = {
+    q: '',
+    format: 'vinyl',
+    key: 'KNMVnsceTtAqbvAVbsPX',
+    secret: 'YjfVFNTeaEqVblcDGkanBBRSWPAeIXBO'
+};
+
 
 function Search () {
     
     /*State managers*/
-    const[error, setError] = useState(null);
-    const[isLoaded, setIsLoaded] = useState(false);
-    const[items, setItems] = useState([]);
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [items, setItems] = useState([]);
     const [search, setSearch] = useState();
 
 
-    useEffect(() => { //runs the fetchItems function after the component mounts
-        fetchItems();
-    }, []); //empty brackets ensures that useEffect will only run after the component mounts
-
-    
-    const fetchItems = async () => { //makes an async call to discogs api
-        const data = await fetch ( //fetches data from discogs api and assigns it to a data variable
-            `https://api.discogs.com/database/search?q=Nirvana`, {headers}
-        );
-
-        const items = await data.json(); //converts data from fetchItems to .json
-        console.log(items);
-
-        setArtists(items.artists); //sets artist state from artist data fetched from discogs API
-    };
 
     return(
         <div>
-            {artists.map(artist => ( //maps through artists array and returns key/value data for each artist
-                <h1 key={artist.id}>{artist.name}</h1> //displays artist name in header
-            ))}
+            <h1>Search</h1>
         </div>
     );
 }
