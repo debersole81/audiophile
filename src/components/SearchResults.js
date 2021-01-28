1e61a883e2a274a4a1a6fb6fcf1a0c162edac2cf
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2021-01-28 13:09:16 -0500

    Fixed return statement and commented out all additional logic

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -2,60 +2,60 @@ import React, { useEffect, useState } from 'react';
 
 function SearchResults () {
 
-    /**State managers*/
-    const [error, setError] = useState(null);
-    const [isLoaded, setIsLoaded] = useState(false);
-    const [items, setItems] = useState([]);
+    // /**State managers*/
+    // const [error, setError] = useState(null);
+    // const [isLoaded, setIsLoaded] = useState(false);
+    // const [items, setItems] = useState([]);
 
-    useEffect(() => {
+    // useEffect(() => {
         
-        /**Buld HTTP query*/
-        const baseURL = 'https://api.discogs.com/database/search?'; //q=Nirvana&key=KNMVnsceTtAqbvAVbsPX&secret=YjfVFNTeaEqVblcDGkanBBRSWPAeIXBO'
-        const query = {
-            q: search,
-            format: 'vinyl',
-            maxResults: '25',
-            key: 'KNMVnsceTtAqbvAVbsPX',
-            secret: 'YjfVFNTeaEqVblcDGkanBBRSWPAeIXBO'
-        } 
+    //     /**Buld HTTP query*/
+    //     const baseURL = 'https://api.discogs.com/database/search?'; //q=Nirvana&key=KNMVnsceTtAqbvAVbsPX&secret=YjfVFNTeaEqVblcDGkanBBRSWPAeIXBO'
+    //     const query = {
+    //         q: search,
+    //         format: 'vinyl',
+    //         maxResults: '25',
+    //         key: 'KNMVnsceTtAqbvAVbsPX',
+    //         secret: 'YjfVFNTeaEqVblcDGkanBBRSWPAeIXBO'
+    //     } 
 
-        /**Build URL*/
-        let url = baseURL;
-        for (let [key, value] of Object.entries(query)) {
-            url = url + key + '=' + value + '&';
-        }
-        url = url.slice(0, -1);
+    //     /**Build URL*/
+    //     let url = baseURL;
+    //     for (let [key, value] of Object.entries(query)) {
+    //         url = url + key + '=' + value + '&';
+    //     }
+    //     url = url.slice(0, -1);
         
-        /**Build request method and headers*/
-        const options = {
-            method: 'GET',
-            headers: {
-                'Accept' : 'application/json',
-                'User-Agent' : 'vinylrecordscatalogue/1.0+localhost:3000'
-            }
-        };
+    //     /**Build request method and headers*/
+    //     const options = {
+    //         method: 'GET',
+    //         headers: {
+    //             'Accept' : 'application/json',
+    //             'User-Agent' : 'vinylrecordscatalogue/1.0+localhost:3000'
+    //         }
+    //     };
 
-        /**Initiate HTTP request to Discogs, then handle response*/
-        fetch(url, options)
-            .then(res => res.json())
-            .then(
-                (result) => {
-                    setItems(result.items);
-                    console.log(result);
-                },
-                (error) => {
-                    setIsLoaded(true);
-                    setError(error);
-                }
-            );
-    }, []); //[] tells useEffect to run only when component mounts
-
-};
+    //     /**Initiate HTTP request to Discogs, then handle response*/
+    //     fetch(url, options)
+    //         .then(res => res.json())
+    //         .then(
+    //             (result) => {
+    //                 setItems(result.items);
+    //                 console.log(result);
+    //             },
+    //             (error) => {
+    //                 setIsLoaded(true);
+    //                 setError(error);
+    //             }
+    //         );
+    // }, []); //[] tells useEffect to run only when component mounts
 
     return(
         <div>
             <h1>Search Results</h1>
         </div>
-    )
+    );
+};
+
 
 export default SearchResults;
