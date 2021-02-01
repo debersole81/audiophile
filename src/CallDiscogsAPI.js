97cbc3d4dc61a73b1544749cd89710e6bf933dc7
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2021-02-01 15:05:04 -0500

    Added build URL logic that iterates through each key/value pair in the buildQueary object to build a url

--- src/CallDiscogsAPI.js
+++ src/CallDiscogsAPI.js
@@ -12,6 +12,17 @@ const callDiscogsAPI = (q, page, per_page) => {
         per_page,
     };
     
+    /**Build URL*/
+    const baseURL = 'https://api.discogs.com/database/search?';
+    
+    let url = baseURL;
+
+    for (let [key, value] of Object.entries(buildQuery)) {
+        url = url + key + '=' + value + '&';
+    }
+    url = url.slice(0, -1);
+
+    
 
 }
 
