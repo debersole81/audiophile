3ed70eade77ee56bf6f388b29ee856bc7b6e7c19
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2021-02-02 06:36:13 -0500

    Removed imports for state hooks and wrapper functions

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from 'react';
-import { defaultQuery, buildURL, defaultGETOptions } from '../apiParameters'
+import React from 'react';
 
 
 function SearchResults () {
