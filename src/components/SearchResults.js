3b1082470016fa9e155c315b893333092ebfb20f
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2021-02-02 06:34:47 -0500

    Deleted useEffect hook and state managers

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -4,29 +4,6 @@ import { defaultQuery, buildURL, defaultGETOptions } from '../apiParameters'
 
 function SearchResults () {
 
-    /**State managers*/
-    const [error, setError] = useState(null);
-    const [isLoaded, setIsLoaded] = useState(false);
-    const [items, setItems] = useState([]);
-
-    useEffect(() => { //this will just do calldiscogs api and return .json
-        // /**Build URL*/        
-        // buildURL(defaultQuery)
-
-        // /**Initiate HTTP request to Discogs, then handle response*/
-        // fetch(url, defaultGETOptions)
-        //     .then(res => res.json()) //abstract this
-        //     .then( //output of callDiscogs api function
-        //         (result) => {
-        //             setItems(result.items);
-        //             console.log(result);
-        //         },
-        //         (error) => {
-        //             setIsLoaded(true);
-        //             setError(error);
-        //         }
-        //     );
-    }, []); //[] tells useEffect to run only when component mounts
 
     return(
         <div>
