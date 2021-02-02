a266c36242f9547abbefa9fce71348702448e311
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2021-02-02 17:08:50 -0500

    Built conditional return statement for rendering state.items if it is not undefined and implemented array.map method in the return statement

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,12 +1,23 @@
 import React from 'react';
 
 
-function SearchResults () {
+function SearchResults (state) {
 
+    console.log(state.items);
+
+    if(state.items !== undefined) {
+        return(
+            <div>
+                {state.items.map((results) =>
+                    <h1>{results.title}</h1>
+                )};
+            </div>
+        );
+    }
 
     return(
         <div>
-            <h1>Search Results</h1>
+            <h1>Search for something!</h1>
         </div>
     );
 };
