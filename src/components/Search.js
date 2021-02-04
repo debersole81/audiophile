56bca9bf1b7b5fdf853cbacc17bf70eafe9ece79
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2021-02-03 19:33:29 -0500

    Deleted compiled bootstrap CSS import

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import callDiscogsAPI from '../CallDiscogsAPI'
 import SearchResults from './SearchResults';
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import Form from 'react-bootstrap/Form';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import Button from 'react-bootstrap/Button'
