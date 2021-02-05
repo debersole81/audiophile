e5a5507846b1c1e7e771868e4852cbbbf1609be8
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2021-02-04 20:12:01 -0500

    Built conditional statement to render return statement if totalPages > 1

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
-function Pagination () {
+function Pagination (props) {
 
+    if(props.totalPages > 1){
+        return(
+        <h1>Pagination links</h1>
+        )
+    };
+
+    return(null);
 
 }
 
