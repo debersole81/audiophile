21a1a550fec66e9e77902140d4634fb9476d41cc
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2021-02-04 20:18:27 -0500

    Declared a variable holding an empty array for to store the page links

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -2,6 +2,12 @@ import React from 'react';
 
 function Pagination (props) {
 
+    /**Build page links*/
+    const pageLinks = []
+
+    
+
+
     if(props.totalPages > 1){
         return(
         <h1>Pagination links</h1>
