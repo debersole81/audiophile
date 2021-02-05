574923fbbdb595266110efaf6bf350f370301fdc
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2021-02-04 20:48:18 -0500

    Built a for loop to push list elements onto the pageLinks array

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -5,8 +5,11 @@ function Pagination (props) {
     /**Build page links*/
     const pageLinks = []
 
-    
-
+    for(let i = 1; i <= props.totalPages + 1; i++){
+        let active = (props.currentPage === i) ? 'active' : '';
+        
+        pageLinks.push(<li key={i} className={`page item ${active}`} >{i}</li>)
+    };
 
     if(props.totalPages > 1){
         return(
