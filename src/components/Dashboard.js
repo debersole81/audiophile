552e9baf6f28a4a07c20b74dde8b7240b364a5f4
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2021-02-16 09:27:43 -0500

    Change Nav.js filename to Header.js. Update component import and reference  in Dashboard.js

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -4,7 +4,7 @@ import Search from "./Search";
 import Collection from "./Collection";
 import WishList from "./WishList";
 import Randomizer from "./Randomizer";
-import Nav from "./Nav";
+import Header from "./Header";
 
 function Dashboard (props) {
 
@@ -28,7 +28,7 @@ function Dashboard (props) {
     if(viewDash) { //if viewDash is true, render links to each component on dashboard path.
         return(
             <div>
-                <Nav viewDashTrue={handleViewDashTrue} viewDashFalse={handleViewDashFalse} />
+                <Header viewDashTrue={handleViewDashTrue} viewDashFalse={handleViewDashFalse} />
                 <Link to="/dashboard/collection" onClick={handleViewDashFalse}> Collection </Link> <br />
                 <Link to="/dashboard/wishlist" onClick={handleViewDashFalse}> Wish List </Link> <br />
                 <Link to="/dashboard/randomizer" onClick={handleViewDashFalse}> Randomizer </Link> <br />
@@ -50,7 +50,7 @@ function Dashboard (props) {
 
     return( //if viewDash is false, remove links to each component on dashboard path
         <div>
-            <Nav viewDashTrue={handleViewDashTrue} viewDashFalse={handleViewDashFalse} />
+            <Header viewDashTrue={handleViewDashTrue} viewDashFalse={handleViewDashFalse} />
             <Switch>
                 <Route exact path="/dashboard/collection" component={Collection} />
                 <Route exact path="/dashboard/wishlist" component={WishList} />
