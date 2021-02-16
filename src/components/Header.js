66c4ec2d6242869e27238fc923cdcaa748aa17c3
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2021-02-16 09:30:09 -0500

    Import LinkContainer from react-router-bootstrap

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { LinkContainer } from 'react-router-bootstrap';
 import { Link } from "react-router-dom";
 
 function Header(props) {
