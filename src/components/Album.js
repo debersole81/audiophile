c379518341492d29bbfe79f87a76ed86b268e48e
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2021-02-18 15:39:12 -0500

    Import and call discogs api release end point wrapper function

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,6 +1,17 @@
 import React from 'react';
+import discogsAPIRelease from '../helper-functions/DiscogsAPIRelease';
 
 function Album () {
+
+    /**Test call to Discogs API release endpoint*/
+    discogsAPIRelease()
+    .then(res => res.json())
+    .then(
+        (result) => {
+            console.log(result);
+        }
+    )
+
 
     return(
         <div>
