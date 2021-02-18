bdb34449fc94c97006236ceee2e4f654b028cf2d
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2021-02-18 13:46:35 -0500

    Move file to a new directory

---
+++ src/helper_functions/CallDiscogsAPI.js
@@ -0,0 +1,38 @@
+/**Abstraction for Discogs API call*/
+
+const callDiscogsAPI = (q, page, per_page = 25) => {
+
+    /**Build Query*/
+    const buildQuery = {
+        q,
+        format: 'album',
+        type: 'master',
+        key: 'KNMVnsceTtAqbvAVbsPX',
+        secret: 'YjfVFNTeaEqVblcDGkanBBRSWPAeIXBO',
+        page,
+        per_page,
+    };
+    
+    /**Build URL*/
+    const baseURL = 'https://api.discogs.com/database/search?';
+    
+    let url = baseURL;
+
+    for (let [key, value] of Object.entries(buildQuery)) {
+        url = url + key + '=' + value + '&';
+    }
+    url = url.slice(0, -1);
+
+    /**Build HTTP request method + headers*/
+    const requestOptions = {
+        method: 'GET',
+        headers: {
+            'Accept' : 'application/json',
+            'User-Agent' : 'vinylrecordscatalogue/1.0+localhost:3000'
+        }
+    };
+
+    return fetch(url, requestOptions);
+}
+
+export default callDiscogsAPI;
