697f100bea772425c42c8c61e2f8d58d5be3a3f2
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2021-02-19 13:25:01 -0500

    Update helper function import and call

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import discogsAPIRelease from '../helper-functions/DiscogsAPIRelease';
+import discogsAPIMaster from '../helper-functions/DiscogsAPIMaster';
+
+//To view the status of changes, setup a route for this component in App.js. Remove when complete.
 
 function Album () {
 
-    /**Test call to Discogs API release endpoint*/
-    discogsAPIRelease()
+    /**Test call to Discogs API master endpoint*/
+    discogsAPIMaster(17243)
     .then(res => res.json())
     .then(
         (result) => {
@@ -18,7 +20,7 @@ function Album () {
             <h1>Album View</h1>
         </div>
     )
-
+    
 };
 
 export default Album;
