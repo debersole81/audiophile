935d9523022b616e8f5d8b34982d4924ef5bc26e
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2021-02-28 15:51:27 -0500

    Update function name and default export

---
+++ src/components/ProtectedComponents.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ProtectedComponents () {
+   
+    return(
+         
+        <h1>This is a protected route</h1>
+    );
+};
+
+export default ProtectedComponents;
