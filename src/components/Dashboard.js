b8a3018e1110cfe5173d06c87dfd49d85154f0de
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2021-03-01 16:03:16 -0500

    Delete component routes

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,10 +1,5 @@
 import React from 'react';
-import { Link, Route, Switch } from 'react-router-dom';
-import Search from './Search';
-import Collection from './Collection';
-import WishList from './WishList';
-import Randomizer from './Randomizer';
-import Header from './Header';
+import { Link } from 'react-router-dom';
 
 function Dashboard(props) {
 
@@ -14,12 +9,6 @@ function Dashboard(props) {
             <Link to='/wishlist'> Wish List </Link> <br />
             <Link to='/randomizer'> Randomizer </Link> <br />
             <Link to='/search'> Search </Link>
-            <Switch>
-                <Route exact path="/dashboard/collection" component={Collection} />
-                <Route exact path="/dashboard/wishlist" component={WishList} />
-                <Route exact path="/dashboard/randomizer" component={Randomizer} />
-                <Route exact path="/dashboard/search" component={Search} />
-            </Switch>
         </div>
     );
 
