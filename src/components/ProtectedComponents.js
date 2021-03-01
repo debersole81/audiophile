f10b12279c839877def0bbc694a3c3c8bd83dc90
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2021-03-01 15:47:27 -0500

    Import components

--- src/components/ProtectedComponents.js
+++ src/components/ProtectedComponents.js
@@ -1,9 +1,17 @@
 import React from "react";
+import Album from './Album';
+import Collection from './Collection';
+import Dashboard from './Dashboard';
+import Header from './Header';
+import Randomizer from './Randomizer';
+import Search from './Search';
+import WishList from './WishList';
 
-function ProtectedComponents () {
+
+function ProtectedComponents (props) {
    
     return(
-         
+        
         <h1>This is a protected route</h1>
     );
 };
