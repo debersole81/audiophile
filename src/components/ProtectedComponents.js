272ec29d188f9e586bc0b252ebdb7f757475563e
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2021-03-01 16:09:30 -0500

    Add component routes for remaining components

--- src/components/ProtectedComponents.js
+++ src/components/ProtectedComponents.js
@@ -16,6 +16,11 @@ function ProtectedComponents(props) {
             <Header />
             <Switch>
                 <Route exact path='/' component={Dashboard} />
+                <Route exact path='/collection' component={Collection} />
+                <Route exact path='/wishlist' component={WishList} />
+                <Route exact path='/randomizer' component={Randomizer} />
+                <Route exact path='/search' component={Search} />
+                <Route exact path='/album' component={Album} />
             </Switch>
         </div>
     );
