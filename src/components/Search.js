f9b7ee480b898796af03b9c41cadf5b57130630f
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2021-03-02 14:13:26 -0500

    Delete search button. Add onSubmit attribute to Form element

--- src/components/Search.js
+++ src/components/Search.js
@@ -13,11 +13,10 @@ function Search(props) {
             <Jumbotron className='text-center'>
                 <h1>Search Albums</h1>
                 <p className='lead text-muted'>Find albums to add to your collection or wishlist.</p>
-                <Form className='form-inline justify-content-center'>
+                <Form className='form-inline justify-content-center' onSubmit={props.handleSearchSubmit}>
                     <Form.Group className='form-group'>
                         <Form.Label srOnly>Search</Form.Label>
-                        <Form.Control className='form-control' type='text' name='search' placeholder='Type an album or artist name.' onChange={props.handleSearch} />
-                        <Button className='btn' variant='secondary' type='submit' size='md' onClick={props.handleSearchSubmit}>Go!</Button>
+                        <Form.Control className='form-control' type='text' name='search' placeholder='Type an album or artist name.' />
                     </Form.Group>
                 </Form>
             </Jumbotron>
