a6df14467401432025df0dba3237358eb47d5496
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2021-03-02 11:57:19 -0500

    Delete search form callback functions

--- src/components/Search.js
+++ src/components/Search.js
@@ -15,184 +15,139 @@ function Search () {
     const paginationDisplayLimit = 5;
 
     /*State variables*/
-    const [search, setSearch] = useState(''); 
-    const [data, setData] = useState([]);
-    const [pagination, setPagination] = useState({});
-    const [minPaginationNum, setMinPaginationNum] = useState(0);
-    const [maxPaginationNum, setMaxPaginationNum] = useState(5);
+    //const [search, setSearch] = useState(''); //done
+    //const [data, setData] = useState([]); //done
+    //const [pagination, setPagination] = useState({}); //done
+    // const [minPaginationNum, setMinPaginationNum] = useState(0);
+    // const [maxPaginationNum, setMaxPaginationNum] = useState(5);
     // const [apiTimeoutElapse, setApiTimeoutElapse] = useState(true);
     // const [error, setError] = useState(null);
     // const [isLoaded, setIsLoaded] = useState(false);
 
     /**Change handlers*/    
     
-    /**Search form change handler*/
-    const handleSearchChange = (e) => {
-        e.preventDefault();
-        
-        setSearch({
-            [e.target.name]: e.target.value
-        });
-        
-        // if(apiTimeoutElapse && search.length > 4) {
-          
-        // //    useEffect(() => {
-
-        // //    }
-           
-        
-        // /**Call discogsAPI (search, per_page, page(use default value as 1))*/
-
-
-
-        //     setApiTimeoutElapse(false);
-
-            
-        // }
-    
-        // setTimeout(() => {
-        //     setApiTimeoutElapse(true);
-        // }, 1000);
-    };
- 
-    /**Search form button handler*/
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        
-        /**Call Discogs API search endpoint*/
-        discogsAPISearch(search.search)
-        .then(res => res.json())
-        .then(
-            (result) => {
-                setData(result.results);
-                setPagination(result.pagination);
-                console.log(result);
-            }
-        );
-
-    };
 
     /**Pagination handlers*/
 
     /**Current page handler. Fires when a page number is clicked in Pagination.js*/
-    const handleCurrentPage = (e) => { 
-        e.preventDefault();
+    // const handleCurrentPage = (e) => { 
+    //     e.preventDefault();
         
-        const pageNum = e.target.id;
+    //     const pageNum = e.target.id;
 
-        /**Call Discogs API search endpoint*/
-        discogsAPISearch(search.search, pageNum)
-        .then(res => res.json())
-        .then(
-            (result) => {
-                setData(result.results);
-                setPagination(result.pagination);
-            }
-        );
+    //     /**Call Discogs API search endpoint*/
+    //     discogsAPISearch(search.search, pageNum)
+    //     .then(res => res.json())
+    //     .then(
+    //         (result) => {
+    //             setData(result.results);
+    //             setPagination(result.pagination);
+    //         }
+    //     );
 
-        /*Scroll to the top of the window after refreshing results*/
-        window.scrollTo(0,0);        
-    };
+    //     /*Scroll to the top of the window after refreshing results*/
+    //     window.scrollTo(0,0);        
+    // };
 
     /**First page handler. Fires when Pagination.First is clicked in Pagination.js */
-    const handleFirstPage = (e) => {
-        e.preventDefault();
+    // const handleFirstPage = (e) => {
+    //     e.preventDefault();
 
-        const pageNum = 1;
+    //     const pageNum = 1;
 
-        /**Set min and max page number values to initial values*/
-        setMinPaginationNum(0);
-        setMaxPaginationNum(5);
+    //     /**Set min and max page number values to initial values*/
+    //     setMinPaginationNum(0);
+    //     setMaxPaginationNum(5);
         
-        /**Call Discogs API search endpoint*/
-        discogsAPISearch(search.search, pageNum)
-        .then(res => res.json())
-        .then(
-            (result) => {
-                setData(result.results);
-                setPagination(result.pagination);
-            }
-        );
+    //     /**Call Discogs API search endpoint*/
+    //     discogsAPISearch(search.search, pageNum)
+    //     .then(res => res.json())
+    //     .then(
+    //         (result) => {
+    //             setData(result.results);
+    //             setPagination(result.pagination);
+    //         }
+    //     );
         
-        /*Scroll to the top of the window after refreshing results*/
-        window.scrollTo(0,0);  
-    };
+    //     /*Scroll to the top of the window after refreshing results*/
+    //     window.scrollTo(0,0);  
+    // };
 
     /**Previous page handler. Fires when Pagination.Prev is clicked in Pagination.js*/
-    const handlePrevPage = (e) => {
-        e.preventDefault();
+    // const handlePrevPage = (e) => {
+    //     e.preventDefault();
 
-        const pageNum = (pagination.page > 1) ? pagination.page - 1 : pagination.page;
+    //     const pageNum = (pagination.page > 1) ? pagination.page - 1 : pagination.page;
 
-        /**Decrement min and max pagination number values by 5*/
-        if((pageNum) % paginationDisplayLimit === 0) {
-            setMinPaginationNum(minPaginationNum - paginationDisplayLimit);
-            setMaxPaginationNum(maxPaginationNum - paginationDisplayLimit)
-        }
+    //     /**Decrement min and max pagination number values by 5*/
+    //     if((pageNum) % paginationDisplayLimit === 0) {
+    //         setMinPaginationNum(minPaginationNum - paginationDisplayLimit);
+    //         setMaxPaginationNum(maxPaginationNum - paginationDisplayLimit)
+    //     }
 
-        /**Call Discogs API search endpoint*/
-        discogsAPISearch(search.search, pageNum)
-        .then(res => res.json())
-        .then(
-            (result) => {
-                setData(result.results);
-                setPagination(result.pagination);
-            }
-        );
+    //     /**Call Discogs API search endpoint*/
+    //     discogsAPISearch(search.search, pageNum)
+    //     .then(res => res.json())
+    //     .then(
+    //         (result) => {
+    //             setData(result.results);
+    //             setPagination(result.pagination);
+    //         }
+    //     );
 
-        /*Scroll to the top of the window after refreshing results*/
-        window.scrollTo(0,0);        
-    };
+    //     /*Scroll to the top of the window after refreshing results*/
+    //     window.scrollTo(0,0);        
+    // };
 
-    /**Next page handler. Fires when Pagination.Next is clicked in Pagination.js*/
-    const handleNextPage = (e) => {
-        e.preventDefault();
+    // /**Next page handler. Fires when Pagination.Next is clicked in Pagination.js*/
+    // const handleNextPage = (e) => {
+    //     e.preventDefault();
 
-        const pageNum = (pagination.page < pagination.pages) ? pagination.page + 1 : pagination.page;
+    //     const pageNum = (pagination.page < pagination.pages) ? pagination.page + 1 : pagination.page;
 
-        /**Incrememnt min and max pagination number values by 5*/
-        if(pageNum > maxPaginationNum) {
-            setMinPaginationNum(minPaginationNum + paginationDisplayLimit);
-            setMaxPaginationNum(maxPaginationNum + paginationDisplayLimit);
-        }
+    //     /**Incrememnt min and max pagination number values by 5*/
+    //     if(pageNum > maxPaginationNum) {
+    //         setMinPaginationNum(minPaginationNum + paginationDisplayLimit);
+    //         setMaxPaginationNum(maxPaginationNum + paginationDisplayLimit);
+    //     }
 
-        /**Call Discogs API search endpoint*/
-        discogsAPISearch(search.search, pageNum)
-        .then(res => res.json())
-        .then(
-            (result) => {
-                setData(result.results);
-                setPagination(result.pagination);
-            }
-        );
+    //     /**Call Discogs API search endpoint*/
+    //     discogsAPISearch(search.search, pageNum)
+    //     .then(res => res.json())
+    //     .then(
+    //         (result) => {
+    //             setData(result.results);
+    //             setPagination(result.pagination);
+    //         }
+    //     );
         
-        /*Scroll to the top of the window after refreshing results*/
-        window.scrollTo(0,0);        
-    };
+    //     /*Scroll to the top of the window after refreshing results*/
+    //     window.scrollTo(0,0);        
+    // };
 
     /**Last page handler. Fires when Pagniation.Last is clicked in Pagination.js*/
-    const handleLastPage = (e) => {
-        e.preventDefault();
+    // const handleLastPage = (e) => {
+    //     e.preventDefault();
 
-        const pageNum = pagination.pages;
+    //     const pageNum = pagination.pages;
 
-        /**Set min and max pagination number values using the total number of pages returned from API call*/
-        setMinPaginationNum(pageNum - paginationDisplayLimit);
-        setMaxPaginationNum(pageNum);
+    //     /**Set min and max pagination number values using the total number of pages returned from API call*/
+    //     setMinPaginationNum(pageNum - paginationDisplayLimit);
+    //     setMaxPaginationNum(pageNum);
 
-        /**Call Discogs API search endpoint*/
-        discogsAPISearch(search.search, pageNum)
-        .then(res => res.json())
-        .then(
-            (result) => {
-                setData(result.results);
-                setPagination(result.pagination);
-            }
-        );
+    //     /**Call Discogs API search endpoint*/
+    //     discogsAPISearch(search.search, pageNum)
+    //     .then(res => res.json())
+    //     .then(
+    //         (result) => {
+    //             setData(result.results);
+    //             setPagination(result.pagination);
+    //         }
+    //     );
         
-        /*Scroll to the top of the window after refreshing results*/
-        window.scrollTo(0,0);        
-    };
+    //     /*Scroll to the top of the window after refreshing results*/
+    //     window.scrollTo(0,0);        
+    // };
 
     /**Passing props*/ //only pass down what each component needs
     const passingProps = {
@@ -208,8 +163,6 @@ function Search () {
         handleLastPage: handleLastPage,        
     };
     
-    //Set conditional to render album (endpoint needs to switch from /search to /album). Usehistory hook?
-
     return(
         <React.Fragment>
             <Jumbotron className='text-center'>
