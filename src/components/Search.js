4012f655ef4209b436d1d87cc76dc564ddea4695
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2021-03-02 12:17:18 -0500

    Updated onChange and onClick attributes with new search props

--- src/components/Search.js
+++ src/components/Search.js
@@ -6,25 +6,23 @@ import Jumbotron from 'react-bootstrap/Jumbotron';
 import Button from 'react-bootstrap/Button'
 
 
-function Search () {
-    
- 
- 
-    return(
+function Search(props) {
+
+    return (
         <React.Fragment>
             <Jumbotron className='text-center'>
                 <h1>Search Albums</h1>
                 <p className='lead text-muted'>Find albums to add to your collection or wishlist.</p>
-                    <Form className='form-inline justify-content-center'>
-                        <Form.Group className='form-group'>
-                            <Form.Label srOnly>Search</Form.Label>
-                            <Form.Control className='form-control' type='text' name='search' placeholder='Type an album or artist name.' onChange={handleSearchChange}/>
-                            <Button className='btn' variant='secondary' type='submit' size='md' onClick={handleSubmit}>Go!</Button>
-                        </Form.Group>
-                    </Form>
-            </Jumbotron>            
-            <SearchResults/>
-            <Pagination/>
+                <Form className='form-inline justify-content-center'>
+                    <Form.Group className='form-group'>
+                        <Form.Label srOnly>Search</Form.Label>
+                        <Form.Control className='form-control' type='text' name='search' placeholder='Type an album or artist name.' onChange={props.handleSearch} />
+                        <Button className='btn' variant='secondary' type='submit' size='md' onClick={props.handleSearchSubmit}>Go!</Button>
+                    </Form.Group>
+                </Form>
+            </Jumbotron>
+            {/* <SearchResults />
+            <Pagination /> */}
         </React.Fragment>
     );
 };
