b3ae71be88c163c0a0cb6bdbb6b927d29af4633c
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2021-03-03 16:52:27 -0500

    Delete helper-function

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,19 +1,6 @@
 import React from 'react';
-import discogsAPIMaster from '../helper-functions/DiscogsAPIMaster';
-
-//To view the status of changes, setup a route for this component in App.js. Remove when complete.
 
 function Album () {
-
-    /**Test call to Discogs API master endpoint*/
-    discogsAPIMaster(17243)
-    .then(res => res.json())
-    .then(
-        (result) => {
-            console.log(result);
-        }
-    )
-
 
     return(
         <div>
