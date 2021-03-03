99194465e4bdc1a3db7266c078b77e25df724309
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2021-03-03 16:50:53 -0500

    Change file name

---
+++ src/helper-functions/DiscogsAPIMasterRelease.js
@@ -0,0 +1,24 @@
+/**Helper function for Discogs API Master Release endpoint call*/
+
+const discogsAPIMasterRelease = (masterId) => {
+
+    /**Build URL*/
+    const baseURL = 'https://api.discogs.com/masters/';
+
+    const url = baseURL + masterId;
+
+    /**Build HTTP request method + headers*/
+    const requestOptions = {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+            'User-Agent': 'vinylrecordscatalogue/1.0+localhost:3000'
+        }
+    };
+
+    return fetch(url, requestOptions);
+
+};
+
+export default discogsAPIMasterRelease;
+
