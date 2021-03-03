c2979d9eb1422eaac451da8ad6ab43dac082f1ca
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2021-03-03 09:11:10 -0500

    Update SearchResultsPagination file name change in import and component element in return statement

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import SearchResults from './SearchResults';
-import Pagination from './Pagination'
+import SearchResultsPagination from './SearchResultsPagination'
 import Form from 'react-bootstrap/Form';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import Button from 'react-bootstrap/Button'
@@ -8,12 +8,14 @@ import Button from 'react-bootstrap/Button'
 
 function Search(props) {
 
+    console.log(props.searchData);
+
     return (
         <React.Fragment>
             <Jumbotron className='text-center'>
                 <h1>Search Albums</h1>
                 <p className='lead text-muted'>Find albums to add to your collection or wishlist.</p>
-                <Form className='form-inline justify-content-center' onSubmit={props.handleSearchSubmit}>
+                <Form className='justify-content-center' onSubmit={props.handleSearchSubmit}>
                     <Form.Group className='form-group'>
                         <Form.Label srOnly>Search</Form.Label>
                         <Form.Control className='form-control' type='text' name='search' placeholder='Type an album or artist name.' value={props.search} onChange={props.handleSearch} />
@@ -21,7 +23,7 @@ function Search(props) {
                 </Form>
             </Jumbotron>
             {/* <SearchResults />
-            <Pagination /> */}
+            <SearchResultsPagination /> */}
         </React.Fragment>
     );
 };
