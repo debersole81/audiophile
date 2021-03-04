81ba12c0971a93ed0cebcec6eb4c3cebe365981e
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2021-03-04 14:23:11 -0500

    Add button with id and onClick attributes

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -13,27 +13,11 @@ function SearchResults(props) {
 
     if (Array.isArray(props.searchData) && props.searchData.length) {
         return (
-
-            //this works
-            // <div>
-            //     {props.searchData.map((results) =>
-            //         <ul key={results.id}>
-            //             <li className='album-links' id={results.master_id} onClick={props.handleAlbumClick}>{results.title}</li>
-            //         </ul>
-            //     )}
-            // </div>
-
-
-            //retrieving attributes seems to make retreiving DOM attributes difficult/if not impossible.
-            //I have been researching for longer than I should be this morning to no avail.
-            //It appears that I might be albe to use a useRef hook, but I don't think that is the answer because the hook is typically used with mutable values.
-            //I could use a button, or just scrap react-bootstrap and use vanilla bootstrap or something else.
-            
             <Container>
                 <Row>
                     {props.searchData.map((results) =>
                         <Col key={results.id} style={{ padding: '1rem' }}>
-                            <Card style={{ width: '12rem' }} id={results.master_id} onClick={props.handleAlbumClick}>
+                            <Card style={{ width: '12rem' }}>
                                 <Card.Img variant='top' src={results.cover_image} style={{ height: '12rem' }} alt='Album Cover Art' />
                                 <Card.Body>
                                     <Card.Title className='text-truncate'>{results.title.split(' - ')[1]}</Card.Title>
@@ -42,8 +26,7 @@ function SearchResults(props) {
                                         {results.label[0]}<br />
                                         {results.year}<br />
                                     </Card.Text>
-                                    {/**This also works, but I don't want a button. I'd prefer that the entire card is clickable*/}
-                                    {/* <Button id={results.master_id} onClick={props.handleAlbumClick}>View</Button> */}
+                                    <Button variant='outline-secondary' size='sm' id={results.master_id} onClick={props.handleAlbumClick} block>View</Button>
                                 </Card.Body>
                             </Card>
                         </Col>
