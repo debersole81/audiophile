47f751e67f1302cea79e6a119959ca9503badbbc
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2021-03-05 09:44:04 -0500

    Import react bootstrap container and build element in return statement

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
 
 function Album( { albumProps: {albumData} } ) {
 
     console.log(albumData);
 
     return (
-        <div>
+        <Container>
             <h1>Album View</h1>
-        </div>
+        </Container>
     )
 
 };
