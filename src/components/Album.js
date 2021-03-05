18a48864a1d6f97d0c640d83f96f7be3c7cf8845
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2021-03-05 09:51:07 -0500

    Import react bootstrap row and col component and build initial layout for album image and details in return statement

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col'
 
-function Album( { albumProps: {albumData} } ) {
+function Album({ albumProps: { albumData } }) {
 
     console.log(albumData);
 
     return (
         <Container>
-            <h1>Album View</h1>
+            <Row xs={2} md={4} lg={6}>
+                <Col>
+                    <h1>Album Image View</h1>
+                </Col>
+                <Col>
+                    <h1>Album Details</h1>
+                </Col>
+            </Row>
         </Container>
     )
 
