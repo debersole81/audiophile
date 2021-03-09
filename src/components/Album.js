4399a1cd50a1cedfda9d03ad79a7b42e11679f5f
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2021-03-09 15:00:39 -0500

    Build callback function to handle more images button click and pass to onClick attribute in button element

--- src/components/Album.js
+++ src/components/Album.js
@@ -15,13 +15,23 @@ function Album({ albumProps: { albumData } }) {
     /**Album images modal component state variables*/
     const [showModal, setShowModal] = useState(false); 
 
+    /**Callback functions*/
+    /**Handle more images button click*/
+    const handleMoreImages = (e) => {
+        e.preventDefault();
+
+        //Set showModal state to true
+        setShowModal(true);
+    };
+
+    console.log(showModal);
 
     return (
         <Container>
             <Row>
                 <Col>
                     <Image src={albumData.images[0].uri} style={{ height: '18rem', width: '18rem' }} alt='Album Cover Art' />
-                    <Button variant='outline-dark' className='mt-2' style={{ width: '18rem' }} block>View More Images</Button>
+                    <Button variant='outline-dark' className='mt-2' style={{ width: '18rem' }} onClick={handleMoreImages} block>View More Images</Button>
                 </Col>
                 <Col>
                     <h3>Album Details</h3>
