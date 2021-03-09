3ed20446587e97513ae56074633cf0f15ea289d2
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2021-03-09 15:33:30 -0500

    Add notes section and a callback to handle modal close

--- src/components/Album.js
+++ src/components/Album.js
@@ -8,6 +8,15 @@ import Button from 'react-bootstrap/Button';
 
 function Album({ albumProps: { albumData } }) {
 
+    //================================================================
+    //                         Notes
+    //================================================================
+    //Need to develop a way to restore previous state (albumData) after a page reload in the browser
+        //Two researched options to accomplish:
+            //Save state loclally via localstorage/indexedDB
+            //Save state at server side  
+    //================================================================
+
     console.log('Render: Album Component');
     console.log(albumData);
 
@@ -16,22 +25,28 @@ function Album({ albumProps: { albumData } }) {
     const [showModal, setShowModal] = useState(false); 
 
     /**Callback functions*/
-    /**Handle more images button click*/
-    const handleMoreImages = (e) => {
+    /**Handle album images modal button click in album component*/
+    const handleShowModal = (e) => {
         e.preventDefault();
 
         //Set showModal state to true
         setShowModal(true);
     };
 
-    console.log(showModal);
+    /**Handle album images modal close button click in AlbumImagesModal component*/
+    const handleCloseModal = (e) => {
+        e.preventDefault();
+
+        //Set showModal to false
+        setShowModal(false);
+    };
 
     return (
         <Container>
             <Row>
                 <Col>
                     <Image src={albumData.images[0].uri} style={{ height: '18rem', width: '18rem' }} alt='Album Cover Art' />
-                    <Button variant='outline-dark' className='mt-2' style={{ width: '18rem' }} onClick={handleMoreImages} block>View More Images</Button>
+                    <Button variant='outline-dark' className='mt-2' style={{ width: '18rem' }} onClick={handleShowModal} block>View More Images</Button>
                 </Col>
                 <Col>
                     <h3>Album Details</h3>
