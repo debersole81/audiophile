4726b3f80289ae7a8d3d94a3952a69ca47b3ab7b
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2021-03-10 15:17:46 -0500

    Build images variable initialized to empty array. Build forEach method to format albumData images as carousel item components. Push each formatted images onto images var

--- src/components/Album.js
+++ src/components/Album.js
@@ -5,6 +5,7 @@ import Col from 'react-bootstrap/Col';
 import Image from 'react-bootstrap/Image';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
+import Carousel from 'react-bootstrap/Carousel';
 
 function Album({ albumProps: { albumData } }) {
 
@@ -35,23 +36,51 @@ function Album({ albumProps: { albumData } }) {
 
     /**Handle modal close button click*/
     const handleCloseModal = () => {
-        
+
         //Set showModal to false
         setShowModal(false);
     };
 
     console.log(showModal);
 
+    /**Prepare images for carousel react bootstrap component*/
+    //Build empty array
+    const images = [];
+
+    //Format images and push onto images array
+    albumData.images.forEach((element, index) => {
+        
+        console.log(element);
+        console.log(index);
+        
+        if (element.type === 'secondary') {
+            images.push(
+                <Carousel.Item>
+                    <img
+                        className='d-block w-100'
+                        src={element.uri}
+                        alt={`More images slide ${index}`}
+                    />
+                </Carousel.Item>
+            )
+        };
+    });
+
+    console.log(images);
+
+
     return (
         <Container>
             <Row>
                 <Col>
                     <Image src={albumData.images[0].uri} style={{ height: '18rem', width: '18rem' }} alt='Album Cover Art' />
-                    <Button variant='outline-dark' className='mt-2' style={{ width: '18rem' }} onClick={handleShowModal} block>Show More Images</Button>
+                    <Button variant='outline-dark' className='mt-2' style={{ width: '18rem' }} onClick={handleShowModal} block>See More Images</Button>
                     <Modal show={showModal} onHide={handleCloseModal} size='lg' aria-labelledby='contained-modal-title-vcenter' centered>
                         <Modal.Header closeButton />
                         <Modal.Body id='contained-modal-title-vcenter'>
-                            <h1>This is a Modal</h1>
+                            <Carousel>
+
+                            </Carousel>
                         </Modal.Body>
                     </Modal>
                 </Col>
