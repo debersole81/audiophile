a7f631b809fd3c208749c9a026849f85557250ed
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2021-03-11 13:28:23 -0500

    Build functional component, and url variables

---
+++ src/helper-functions/DiscogsAPIRelease.js
@@ -0,0 +1,14 @@
+/**Helper function for Discgos API Release endpoint call*/
+
+const discogsAPIRelease = (main_release) => {
+    
+    /*Build URL*/
+    const baseURL = 'https://api.discogs.com/releases/';
+
+    let url = baseURL + main_release;
+
+
+};
+
+export default discogsAPIRelease;
+
