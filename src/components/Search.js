79e9cb769739ce81749fecd8d6235526903f08ca
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2021-03-16 10:02:40 -0400

    Add row and column for search albums text. Apply class attributes

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,6 +3,9 @@ import SearchResults from './SearchResults';
 import SearchResultsPagination from './SearchResultsPagination'
 import Form from 'react-bootstrap/Form';
 import Jumbotron from 'react-bootstrap/Jumbotron';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import '../App.css';
 
 
 function Search(props) {
@@ -11,8 +14,12 @@ function Search(props) {
 
     return (
         <React.Fragment>
-            <Jumbotron className='text-center'>
-                <h1>Search Albums</h1>
+            <Jumbotron className='search-jumbotron'>
+                <Row className='row'>
+                    <Col className='col'>
+                        <h1>Search Albums</h1>
+                    </Col>
+                </Row>
                 <p className='lead text-muted'>Find albums to add to your collection or wishlist.</p>
                 <Form className='justify-content-center' onSubmit={props.searchProps.handleSearchSubmit}>
                     <Form.Group className='form-group'>
