2ad0d4a1824864e8d47424f16639002471856bbe
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2021-03-16 14:18:47 -0400

    Update className attribute props on card elements

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -4,9 +4,10 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
+import CardColumns from 'react-bootstrap/CardColumns';
 import Button from 'react-bootstrap/Button';
 
-function SearchResults({searchData, handleAlbumClick}) {
+function SearchResults({ searchData, handleAlbumClick }) {
 
     console.log('Render: Search Results Component');
     console.log(searchData);
@@ -16,9 +17,9 @@ function SearchResults({searchData, handleAlbumClick}) {
             <Container>
                 <Row>
                     {searchData.map((results) =>
-                        <Col key={results.id} style={{ padding: '1rem' }}>
-                            <Card style={{ width: '12rem' }}>
-                                <Card.Img variant='top' src={results.cover_image} style={{ height: '12rem' }} alt='Album Cover Art' />
+                        <Col key={results.id} className='search-results-col'>
+                            <Card className='search-results-card'>
+                                <Card.Img className='search-results-card-img' variant='top' src={results.cover_image} alt='Album Cover Art' />
                                 <Card.Body>
                                     <Card.Title className='text-truncate'>{results.title.split(' - ')[1]}</Card.Title>
                                     <Card.Subtitle className='mb-2 text-truncate text-muted'>{results.title.split(' - ')[0]}</Card.Subtitle>
