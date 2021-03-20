ea40e48a8e59e5a10050f96ea7f032d1f6355492
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2021-03-20 06:23:40 -0400

    Wrap album details in row and col elements

--- src/components/Album.js
+++ src/components/Album.js
@@ -81,13 +81,15 @@ function Album({ albumProps: { albumData } }) {
                 </Col>
                 <Col className='col' xs={12} s={12} md={6} lg={6}>
                     <h2 className='mt-3 mb-2'>{albumData.title}</h2>
-                    <h4 className='text-muted'>{albumData.artists[0].name}</h4>
-                    <div>
-                        <h5>Album Details</h5>
-                        <p style={{ margin: 0 }}>{albumData.formats[0].name}</p>
-                        <p style={{ margin: 0 }}>{albumData.labels[0].name} - {albumData.labels[0].catno}</p>
-                        <p style={{ margin: 0 }}>{albumData.country} &#8226; {albumData.year}</p>
-                    </div>
+                    <h3 className='text-muted'>{albumData.artists[0].name}</h3>
+                    <Row>
+                        <Col>
+                            <h5>Album Details</h5>
+                            <p style={{ margin: 0 }}>{albumData.formats[0].name}</p>
+                            <p style={{ margin: 0 }}>{albumData.labels[0].name} - {albumData.labels[0].catno}</p>
+                            <p style={{ margin: 0 }}>{albumData.country} &#8226; {albumData.year}</p>
+                        </Col>
+                    </Row>
                 </Col>
             </Row>
         </Container>
