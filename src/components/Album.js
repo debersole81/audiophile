cbfde503a4656d7b755debd6a5e964ee6b38832f
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2021-03-23 11:08:08 -0400

    Import AlbumTracks component

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import '../App.css';
+import AlbumTracks from '../components/AlbumTracks'
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
