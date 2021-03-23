69d5d6f1b97f0b83c896f622ca3d0058f79a8efb
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2021-03-23 11:06:50 -0400

    Build functional component and add component build notes

---
+++ src/components/AlbumTracks.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+/**
+ * Pass albumData props
+ * Destructure props
+ * Use bootstrap table to display track number, track title, and track time
+ * tracklist data is an array of objects
+ * will need a way to map through each object and add it to the bootstrap table component (forEach? .map?)
+ */
+
+console.log('Render: AlbumTracks Component');
+
+function AlbumTracks() {
+
+    return (
+        <h1>This will hold the album tracks</h1>
+    );
+};
+
+export default AlbumTracks;
+
