f5d74f55bff2f7fff5bb82e62bc5beb046970eb2
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2021-03-24 10:41:04 -0400

    Delete notes

--- src/components/AlbumTracks.js
+++ src/components/AlbumTracks.js
@@ -5,15 +5,6 @@ import Row from 'react-bootstrap/Row';
 import Container from 'react-bootstrap/Container';
 import Table from 'react-bootstrap/Table';
 
-/**
- * Pass albumData props
- * Destructure props
- * Use bootstrap table to display position, title, and duration
- * tracklist data is an array of objects
- * will need a way to map through each object and add it to the bootstrap table component (forEach? .map?)
- * use a conditional to display a - if track duration value is null 
-*/
-
 function AlbumTracks(albumData) {
 
     console.log('Render: AlbumTracks Component');
