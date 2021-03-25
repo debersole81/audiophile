0942db5ee99a9ed7e9ea2d484de720b6820089b7
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2021-03-24 20:06:43 -0400

    Build wrapper function with url params, method, and header objects

---
+++ src/helper-functions/DiscogsAPIMasterReleaseVersions.js
@@ -0,0 +1,24 @@
+/**Helper function for Discogs API Master Release Versions endpoint call*/
+
+const discogsAPIMasterReleaseVersions = (masterId) => {
+
+    /**Build URL*/
+    const baseURL = 'https://api.discogs.com/masters/';
+
+    let url = baseURL + masterId;
+
+    /**Build HTTP request method + headers*/
+    const requestOptions = {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+            'Authorization': 'Discogs key=KNMVnsceTtAqbvAVbsPX, secret=YjfVFNTeaEqVblcDGkanBBRSWPAeIXBO',
+            'User-Agent': 'vinylrecordscatalogue/1.0+localhost:3000'
+        }
+    };
+
+    return fetch(url, requestOptions);
+
+};
+
+export default discogsAPIMasterReleaseVersions;
