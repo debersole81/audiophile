9167c26536814a02adb7107ade90d10588384e60
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2021-03-25 15:32:56 -0400

    Import AlbumVersions component

--- src/components/AlbumVersionsHeader.js
+++ src/components/AlbumVersionsHeader.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import '../App.css';
+import AlbumVersions from '../components/AlbumVersions';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
 
 function AlbumVersionsHeader() {
     return (
-        null
+        <Container>
+            <Row>
+                <Col>
+                    <h5>Album Versions</h5>
+                </Col>
+                <Col>
+                    <Button></Button>
+                </Col>
+            </Row>
+        </Container>
     )
 };
 
