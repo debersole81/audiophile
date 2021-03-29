9d878e2b36f7a823e5a7d7c567e09aa7b9122246
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2021-03-29 06:40:16 -0400

    Add notes

--- src/components/AlbumVersionsHeader.js
+++ src/components/AlbumVersionsHeader.js
@@ -6,6 +6,23 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 
+/**Notes
+ * Build button to 'View Album Versions'
+ * Button will fire a callback in ProtectedComponents.js and provide access to master_id
+ * Protected components callback will... 
+ * ...call Discogs Master Release Versions EP
+ * ...setAlbumVersions data
+ * ...setAlbumVersions pagination data (but do I need this if I scroll the overflow?)
+ * Build button to 'Hide Album Versions'
+ * Button will clear albumVersions data in Protected component
+ * Button will clear albumVersions data in Protected component
+ * View/Hide buttons will render based on an inline conditional that ...
+ * ...will evaluate albumVersions state object
+ * ...if albumVersions in falsy, render show button
+ * ...if albumVersions is truthy, render hide button 
+ */
+
+
 function AlbumVersionsHeader(props) {
 
     console.log(props);
