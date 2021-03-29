9d40476f941d984f3270da75ed63bf70e9cdd843
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2021-03-29 14:17:45 -0400

    Build pagionation elements in return statement

--- src/components/AlbumVersionsPagination.js
+++ src/components/AlbumVersionsPagination.js
@@ -1,8 +1,23 @@
 import React from "react";
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Pagination from 'react-bootstrap/Pagination';
 
 function AlbumVersionsPagination() {
 
-    return (null);
+    return (
+        <Row className='row'>
+            <Col className='col'>
+                <Pagination>
+                    <Pagination.First>First</Pagination.First>
+                    <Pagination.Prev>Previous</Pagination.Prev>
+                    <Pagination.Item>1</Pagination.Item>
+                    <Pagination.Next>Next</Pagination.Next>
+                    <Pagination.Last>Last</Pagination.Last>
+                </Pagination>
+            </Col>
+        </Row>
+    );
 };
 
 export default AlbumVersionsPagination;
