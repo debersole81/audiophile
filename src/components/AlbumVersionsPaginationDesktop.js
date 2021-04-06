733958c5526a1d819450358ebef73b95574ff99f
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2021-04-06 10:00:17 -0400

    Build functional component

---
+++ src/components/AlbumVersionsPaginationDesktop.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function AlbumVersionsPaginationDesktop() {
+
+    return(
+        <h1>Desktop version</h1>
+    );
+};
+
+export default AlbumVersionsPaginationDesktop;
