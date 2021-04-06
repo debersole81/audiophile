562db1a0e56691330f9882b700d1791b13e3f551
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2021-04-06 10:04:45 -0400

    Destructure props

--- src/components/AlbumVersionsPaginationDesktop.js
+++ src/components/AlbumVersionsPaginationDesktop.js
@@ -1,6 +1,20 @@
 import React from 'react';
 
-function AlbumVersionsPaginationDesktop() {
+function AlbumVersionsPaginationDesktop(props) {
+
+    console.log('Render: AlbumVersionsPaginationDesktop Component');
+
+    /**Destructure props*/
+    const { albumVersionsPagination } = props.albumVersionsPaginationProps;
+    const { albumVersionsMinPages } = props.albumVersionsPaginationProps;
+    const { albumVersionsMaxPages } = props.albumVersionsPaginationProps;
+    const { handleCurrentAlbumVersionsPage } = props.albumVersionsPaginationProps;
+    const { handleFirstAlbumVersionsPage } = props.albumVersionsPaginationProps;
+    const { handlePreviousAlbumVersionsPage } = props.albumVersionsPaginationProps;
+    const { handleNextAlbumVersionsPage } = props.albumVersionsPaginationProps;
+    const { handleLastAlbumVersionsPage } = props.albumVersionsPaginationProps;
+
+    
 
     return(
         <h1>Desktop version</h1>
