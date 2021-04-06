91fbf01fe2a1f09311bc538a253966de8abedb34
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2021-04-06 14:32:27 -0400

    Import useViewPort hook

--- src/components/AlbumVersionsPaginationWrapper.js
+++ src/components/AlbumVersionsPaginationWrapper.js
@@ -1,12 +1,13 @@
 import React from "react";
 import AlbumVersionsPaginationDesktop from '../components/AlbumVersionsPaginationDesktop';
+import { useViewPort } from '../custom-hooks/useViewPort';
 
 function AlbumVersionsPaginationWrapper(props) {
 
     console.log('Render: AlbumVersionsPaginationWrapper Component');
 
     return (
-        <AlbumVersionsPaginationDesktop {...props}/>
+        <AlbumVersionsPaginationDesktop {...props} />
     );
 };
 
