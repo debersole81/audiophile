124b5f02b2f20329383e3b0ef4c7695906d5b675
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2021-04-06 14:30:12 -0400

    Build listener inside of useEffect hook to set width state variable on window resize. Return width and export named function.

--- src/custom-hooks/useViewPort.js
+++ src/custom-hooks/useViewPort.js
@@ -3,8 +3,22 @@ import { useState, useEffect } from 'react';
 /**Custom hook to render components based upon viewport size */
 
 const useViewPort = () => {
-    /*Declare state variable to hold viewport width*/
+    /** State variable to hold viewport width */
     const [width, setWidth] = useState(window.innerWidth);
 
+    /** Listen for viewport width changes when the component mounts */
+    useEffect(() => {
+        //Handle window resize
+        const handleWindowResize = () => {
+            setWidth(window.innerWidth);
+        }
 
-}
+        //Listen to viewport width changes and fire window resize callback
+        window.addEventListener('resize', handleWindowResize);
+    }, []);
+
+    /** Return width for use in component */
+    return { width };
+};
+
+export { useViewPort };
