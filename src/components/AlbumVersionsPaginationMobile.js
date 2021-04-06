09568bd051f6c4ebba5a07eb66a2b0c85da07623
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2021-04-06 15:22:39 -0400

    Destructure props

--- src/components/AlbumVersionsPaginationMobile.js
+++ src/components/AlbumVersionsPaginationMobile.js
@@ -5,9 +5,20 @@ import Col from 'react-bootstrap/Col';
 import Pagination from 'react-bootstrap/Pagination';
 
 function AlbumVersionsPaginationMobile(props) {
-    
+
     console.log('Render: AlbumVersionPaginationMobile Component');
-    
+
+    /** Destructure props */
+    const { albumVersionsPagination } = props.albumVersionsPaginationProps;
+    const { albumVersionsMinPages } = props.albumVersionsPaginationProps;
+    const { albumVersionsMaxPages } = props.albumVersionsPaginationProps;
+    const { handleCurrentAlbumVersionsPage } = props.albumVersionsPaginationProps;
+    const { handleFirstAlbumVersionsPage } = props.albumVersionsPaginationProps;
+    const { handlePreviousAlbumVersionsPage } = props.albumVersionsPaginationProps;
+    const { handleNextAlbumVersionsPage } = props.albumVersionsPaginationProps;
+    const { handleLastAlbumVersionsPage } = props.albumVersionsPaginationProps;
+
+
     return (
         <h1>Mobile Pagination</h1>
     );
