f1cf62fbf52e223f2394edf127569742cc4302c6
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2021-04-06 15:12:17 -0400

    Build functional component

---
+++ src/components/AlbumVersionsPaginationMobile.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function AlbumVersionsPaginationMobile() {
+    return (
+        <h1>Mobile Pagination</h1>
+    );
+};
+
+export default AlbumVersionsPaginationMobile;
