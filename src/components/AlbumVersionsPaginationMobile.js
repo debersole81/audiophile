0c2d95f818008d4ef586c2a2152af3fff59ed039
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2021-04-06 15:21:47 -0400

    Import react bootsrap components and app.css

--- src/components/AlbumVersionsPaginationMobile.js
+++ src/components/AlbumVersionsPaginationMobile.js
@@ -1,6 +1,13 @@
 import React from 'react';
+import "../App.css";
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Pagination from 'react-bootstrap/Pagination';
 
-function AlbumVersionsPaginationMobile() {
+function AlbumVersionsPaginationMobile(props) {
+    
+    console.log('Render: AlbumVersionPaginationMobile Component');
+    
     return (
         <h1>Mobile Pagination</h1>
     );
