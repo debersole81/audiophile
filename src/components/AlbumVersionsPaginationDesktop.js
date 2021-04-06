2e220269245b686fff3ad5c054d0f9fd257c9e95
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2021-04-06 10:12:37 -0400

    Import css, row, col, and pagination

--- src/components/AlbumVersionsPaginationDesktop.js
+++ src/components/AlbumVersionsPaginationDesktop.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import '../App.css';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Pagination from 'react-bootstrap/Pagination';
 
 function AlbumVersionsPaginationDesktop(props) {
 
