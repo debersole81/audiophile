c0f9163c53f8f9322f4f1e5e749583c4a8e5f4da
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2021-04-06 10:38:18 -0400

    Declare state variable to hold viewport width

--- src/custom-hooks/useViewPort.js
+++ src/custom-hooks/useViewPort.js
@@ -3,4 +3,8 @@ import { useState, useEffect } from 'react';
 /**Custom hook to render components based upon viewport size */
 
 const useViewPort = () => {
+    /*Declare state variable to hold viewport width*/
+    const [width, setWidth] = useState(window.innerWidth);
+
+
 }
