01ec91712158ecb0949d9e482d08d462fecfcc9a
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2021-04-06 14:49:17 -0400

    Destructure width prop from useViewPort hook

--- src/components/AlbumVersionsPaginationWrapper.js
+++ src/components/AlbumVersionsPaginationWrapper.js
@@ -6,6 +6,11 @@ function AlbumVersionsPaginationWrapper(props) {
 
     console.log('Render: AlbumVersionsPaginationWrapper Component');
 
+    /** Destructure width variable from useViewPort hook */
+    const { width } = useViewPort();
+
+    console.log(width);
+
     return (
         <AlbumVersionsPaginationDesktop {...props} />
     );
