042a77e690a1614c411697bed7d2a530f433e6ea
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2021-04-08 16:08:51 -0400

    Import bootstrap components

--- src/components/SearchResultsPaginationMobile.js
+++ src/components/SearchResultsPaginationMobile.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import '../App.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Pagination from 'react-bootstrap/Pagination'
 
-function SearchResultsPaginationMobile () {
+function SearchResultsPaginationMobile (props) {
+    
     console.log('Render: SearchResultsPaginationMobile Component')
-
+    console.log(props);
+    
     return(
         <h1>Mobile pagination</h1>
     );
