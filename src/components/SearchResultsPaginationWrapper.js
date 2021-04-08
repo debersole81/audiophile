bcd5ecacf30cf07f60b62156fc34b3208eed1484
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2021-04-08 13:52:37 -0400

    Build functional component

---
+++ src/components/SearchResultsPaginationWrapper.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { useViewPort } from '../custom-hooks/useViewPort'
+
+function SeaarchResultsPaginationWrapper() {
+    console.log('Render: SearchResultsPaginationWrapper Component');
+
+    return (
+        <h1>This is a wrapper</h1>
+    );
+};
+
+export default SeaarchResultsPaginationWrapper;
