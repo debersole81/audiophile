8d5f5da27f4943484d3990b4c3636b28b008fe5c
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2021-04-08 15:10:43 -0400

    Destructure props and update attribute values props in return statement

--- src/components/SearchResultsPaginationDesktop.js
+++ src/components/SearchResultsPaginationDesktop.js
@@ -5,33 +5,44 @@ import Pagination from 'react-bootstrap/Pagination';
 function SearchResultsPaginationDesktop(props) {
 
     console.log('Render: SearchResultsPagination Component');
+    console.log(props);
 
-    /**Build pages array*/
+    /** Destructure props */
+    const { searchResultsPagination } = props.searchResultsPaginationProps;
+    const { searchResultsMinPages } = props.searchResultsPaginationProps;
+    const { searchResultsMaxPages } = props.searchResultsPaginationProps;
+    const { handleCurrentSearchResultsPage } = props.searchResultsPaginationProps;
+    const { handleFirstSearchResultsPage } = props.searchResultsPaginationProps;
+    const { handlePreviousSearchResultsPage } = props.searchResultsPaginationProps;
+    const { handleNextSearchResultsPage } = props.searchResultsPaginationProps;
+    const { handleLastSearchResultsPage } = props.searchResultsPaginationProps;
+
+    /** Build pages array */
     //Variable to hold active page
-    const activePage = props.searchResultsPagination.page
+    const activePage = searchResultsPagination.page
 
     //Empty array to store formatted page numbers
     const pages = []
 
     //Loop over pages and apply formatting
-    for (let i = 1; i <= props.searchResultsPagination.pages; i++) {
+    for (let i = 1; i <= searchResultsPagination.pages; i++) {
         //Push page numbers onto pages array
         pages.push(
-            <Pagination.Item key={i} id={i} className={(i === activePage ? 'active' : null)} onClick={props.handleCurrentSearchResultsPage}>
+            <Pagination.Item key={i} id={i} className={(i === activePage ? 'active' : null)} onClick={handleCurrentSearchResultsPage}>
                 {i}
             </Pagination.Item>
         )
     };
 
-    if (props.searchResultsPagination.pages > 1) {
+    if (searchResultsPagination.pages > 1) {
         return (
             <React.Fragment>
                 <Pagination className='mt-3 flex-wrap justify-content-center'>
-                    <Pagination.First className={props.searchResultsPagination.page === 1 ? 'disabled' : ''} onClick={props.handleFirstSearchResultsPage} />
-                    <Pagination.Prev className={props.searchResultsPagination.page === 1 ? 'disabled' : ''} onClick={props.handlePreviousSearchResultsPage} />
-                    {pages.map((page) => (page.props.id < props.searchResultsMaxPages + 1 && page.props.id > props.searchResultsMinPages) ? page : null)}
-                    <Pagination.Next className={props.searchResultsPagination.page === props.searchResultsPagination.pages ? 'disabled' : ''} onClick={props.handleNextSearchResultsPage} />
-                    <Pagination.Last className={props.searchResultsPagination.page === props.searchResultsPagination.pages ? 'disabled' : ''} onClick={props.handleLastSearchResultsPage} />
+                    <Pagination.First className={searchResultsPagination.page === 1 ? 'disabled' : ''} onClick={handleFirstSearchResultsPage} />
+                    <Pagination.Prev className={searchResultsPagination.page === 1 ? 'disabled' : ''} onClick={handlePreviousSearchResultsPage} />
+                    {pages.map((page) => (page.props.id < searchResultsMaxPages + 1 && page.props.id > searchResultsMinPages) ? page : null)}
+                    <Pagination.Next className={searchResultsPagination.page === searchResultsPagination.pages ? 'disabled' : ''} onClick={handleNextSearchResultsPage} />
+                    <Pagination.Last className={searchResultsPagination.page === searchResultsPagination.pages ? 'disabled' : ''} onClick={handleLastSearchResultsPage} />
                 </Pagination>
             </React.Fragment>
         )
