678dc0a1b4d1907bf0b306eb0051e48d27108ada
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2021-04-08 16:58:55 -0400

    Destrucutre props

--- src/components/SearchResultsPaginationMobile.js
+++ src/components/SearchResultsPaginationMobile.js
@@ -5,12 +5,23 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Pagination from 'react-bootstrap/Pagination'
 
-function SearchResultsPaginationMobile (props) {
-    
+function SearchResultsPaginationMobile(props) {
+
     console.log('Render: SearchResultsPaginationMobile Component')
     console.log(props);
+
+    /** Destructure props */
+    const { searchResultsPagination } = props.searchResultsPaginationProps;
+    const { searchResultsMinPagesMobile } = props.searchResultsPaginationProps;
+    const { searchResultsMaxPagesMobile } = props.searchResultsPaginationProps;
+    const { handleCurrentSearchResultsPage } = props.searchResultsPaginationProps;
+    const { handlePreviousSearchResultsPageMobile } = props.searchResultsPaginationProps;
+    const { handleNextSearchResultsPageMobile } = props.searchResultsPaginationProps;
+
     
-    return(
+
+
+    return (
         <h1>Mobile pagination</h1>
     );
 };
