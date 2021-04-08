ebf9f4fcace3f95974d76266e81c40839381968b
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2021-04-08 13:56:16 -0400

    Destructure width variable and declare breakpoint variable

--- src/components/SearchResultsPaginationWrapper.js
+++ src/components/SearchResultsPaginationWrapper.js
@@ -4,6 +4,12 @@ import { useViewPort } from '../custom-hooks/useViewPort'
 function SeaarchResultsPaginationWrapper() {
     console.log('Render: SearchResultsPaginationWrapper Component');
 
+    /** Destructure width variable from useViewPort hook */
+    const { width } = useViewPort();
+
+    /** Declare variable for minimum breakpoint value */
+    const breakpoint = 381;
+
     return (
         <h1>This is a wrapper</h1>
     );
