6704afb571b049e7108e9ecf1df7289c2b9415f0
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2021-04-08 15:30:03 -0400

    Build functional component

---
+++ src/components/SearchResultsPaginationMobile.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function SearchResultsPaginationMobile () {
+    console.log('Render: SearchResultsPaginationMobile Component')
+
+    return(
+        <h1>Mobile pagination</h1>
+    );
+};
+
+export default SearchResultsPaginationMobile;
