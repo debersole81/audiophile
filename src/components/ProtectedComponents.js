3bda5f4d8ad506c40c2ba73afecf5e1726069fe8
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2021-04-12 13:39:04 -0400

    Pass handleAlbumClick to the albumVersionsProps object

--- src/components/ProtectedComponents.js
+++ src/components/ProtectedComponents.js
@@ -53,34 +53,7 @@ function ProtectedComponents(props) {
 
 
     /* #region Callback Functions */
-    /** Search component callback functions */
-    /* Handle search form input field */
-    const handleSearch = (({ target }) => {
-        setSearch(target.value);
-    });
-
-    /* Handle search form submit */
-    const handleSearchSubmit = (e) => {
-        e.preventDefault();
-
-        //Clear searchResultsMinPages & searchResultsMaxPages state
-        setSearchResultsMinPages(0);
-        setSearchResultsMaxPages(5);
-
-        //Call Discogs API Search endpoint
-        //Set searchData
-        //Set searchResultsPagination
-        DiscogsAPISearch(search)
-            .then(res => res.json())
-            .then(
-                (result) => {
-                    setSearchData(result.results);
-                    setSearchResultsPagination(result.pagination);
-                }
-            );
-    };
-
-    /** SearchResults component callback functions */
+    /** Callbacks used in multiple components */
     /* Handle album click */
     const handleAlbumClick = (e) => {
         e.preventDefault();
@@ -117,6 +90,34 @@ function ProtectedComponents(props) {
     console.log(albumData);
     console.log(albumMasterData);
 
+    /** Search component callback functions */
+    /* Handle search form input field */
+    const handleSearch = (({ target }) => {
+        setSearch(target.value);
+    });
+
+    /* Handle search form submit */
+    const handleSearchSubmit = (e) => {
+        e.preventDefault();
+
+        //Clear searchResultsMinPages & searchResultsMaxPages state
+        setSearchResultsMinPages(0);
+        setSearchResultsMaxPages(5);
+
+        //Call Discogs API Search endpoint
+        //Set searchData
+        //Set searchResultsPagination
+        DiscogsAPISearch(search)
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    setSearchData(result.results);
+                    setSearchResultsPagination(result.pagination);
+                }
+            );
+    };
+
+    /** SearchResults component callback functions */
     /** SearchResultsPagination component callback functions -- Mobile */
     /* Handle previous page button click */
     const handlePreviousSearchResultsPageMobile = (e) => {
@@ -556,6 +557,7 @@ function ProtectedComponents(props) {
     const albumVersionsProps = {
         handleViewAlbumVersions,
         handleHideAlbumVersions,
+        handleAlbumClick,
         albumVersionsData,
     };
 
