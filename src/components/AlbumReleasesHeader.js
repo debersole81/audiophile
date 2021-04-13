2184c78c86bea222c8c484b6c1fed6fa516404e3
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2021-04-13 10:55:53 -0400

    Update className attribute values to album-releases name change

--- src/components/AlbumReleasesHeader.js
+++ src/components/AlbumReleasesHeader.js
@@ -34,12 +34,12 @@ function AlbumReleasesHeader(props) {
     if (Object.keys(albumReleasesData).length === 0 && albumReleasesData.constructor === Object) {
         return (
             <Container>
-                <Row className='row album-versions-header-row'>
-                    <Col className='col album-versions-header-h5-col' xs={12} s={12} lg={6}>
-                        <h5 className='album-versions-header-thead'>Album Releases</h5>
+                <Row className='row album-releases-header-row'>
+                    <Col className='col album-releases-header-h5-col' xs={12} s={12} lg={6}>
+                        <h5 className='album-releases-header-thead'>Album Releases</h5>
                     </Col>
-                    <Col className='col album-versions-header-button-col' xs={12} s={12} lg={6}>
-                        <Button size='sm' variant='outline-light' className='album-versions-header-button' onClick={handleViewAlbumReleases}>SHOW RELEASES</Button>
+                    <Col className='col album-releases-header-button-col' xs={12} s={12} lg={6}>
+                        <Button size='sm' variant='outline-light' className='album-releases-header-button' onClick={handleViewAlbumReleases}>SHOW RELEASES</Button>
                     </Col>
                 </Row>
             </Container>
@@ -48,12 +48,12 @@ function AlbumReleasesHeader(props) {
 
     return (
         <Container>
-            <Row className='row album-versions-header-row'>
-                <Col className='col album-versions-header-h5-col' xs={12} s={12} lg={6}>
-                    <h5 className='album-versions-header-thead'>Album Releases</h5>
+            <Row className='row album-releases-header-row'>
+                <Col className='col album-releases-header-h5-col' xs={12} s={12} lg={6}>
+                    <h5 className='album-releases-header-thead'>Album Releases</h5>
                 </Col>
-                <Col className='col album-versions-header-button-col' xs={12} s={12} lg={6}>
-                    <Button size='sm' variant='outline-light' className='album-versions-header-button' onClick={handleHideAlbumReleases}>HIDE RELEASES</Button>
+                <Col className='col album-releases-header-button-col' xs={12} s={12} lg={6}>
+                    <Button size='sm' variant='outline-light' className='album-releases-header-button' onClick={handleHideAlbumReleases}>HIDE RELEASES</Button>
                 </Col>
             </Row>
             <AlbumReleases albumReleasesProps={props.albumReleasesProps} albumReleasesPaginationProps={props.albumReleasesPaginationProps} />
