0cd703c551a7dd420ea83bccd6775d77abbd804f
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2021-04-13 10:13:23 -0400

    Apply album releases pagination name change to props destructuring, attribute names, and values

--- src/components/AlbumReleasesPaginationDesktop.js
+++ src/components/AlbumReleasesPaginationDesktop.js
@@ -6,45 +6,45 @@ import Pagination from 'react-bootstrap/Pagination';
 
 function AlbumReleasesPaginationDesktop(props) {
 
-    console.log('Render: AlbumVersionsPaginationDesktop Component');
+    console.log('Render: AlbumReleasesPaginationDesktop Component');
 
     /** Destructure props */
-    const { albumVersionsPagination } = props.albumVersionsPaginationProps;
-    const { albumVersionsMinPages } = props.albumVersionsPaginationProps;
-    const { albumVersionsMaxPages } = props.albumVersionsPaginationProps;
-    const { handleCurrentAlbumVersionsPage } = props.albumVersionsPaginationProps;
-    const { handleFirstAlbumVersionsPage } = props.albumVersionsPaginationProps;
-    const { handlePreviousAlbumVersionsPage } = props.albumVersionsPaginationProps;
-    const { handleNextAlbumVersionsPage } = props.albumVersionsPaginationProps;
-    const { handleLastAlbumVersionsPage } = props.albumVersionsPaginationProps;
+    const { albumReleasesPagination } = props.albumReleasesPaginationProps;
+    const { albumReleasesMinPages } = props.albumReleasesPaginationProps;
+    const { albumReleasesMaxPages } = props.albumReleasesPaginationProps;
+    const { handleCurrentAlbumReleasesPage } = props.albumReleasesPaginationProps;
+    const { handleFirstAlbumReleasesPage } = props.albumReleasesPaginationProps;
+    const { handlePreviousAlbumReleasesPage } = props.albumReleasesPaginationProps;
+    const { handleNextAlbumReleasesPage } = props.albumReleasesPaginationProps;
+    const { handleLastAlbumReleasesPage } = props.albumReleasesPaginationProps;
 
     /** Build pages array */
     //Variable to hold active page
-    const activePage = albumVersionsPagination.page;
+    const activePage = albumReleasesPagination.page;
 
     //Empty array to store formatted page numbers
     const pages = [];
 
     //Loop over pages and apply formatting
-    for (let i = 1; i <= albumVersionsPagination.pages; i++) {
+    for (let i = 1; i <= albumReleasesPagination.pages; i++) {
         //Push page numbers onto pages array
         pages.push(
-            <Pagination.Item key={i} id={i} className={(i === activePage ? 'active' : null)} onClick={handleCurrentAlbumVersionsPage}>
+            <Pagination.Item key={i} id={i} className={(i === activePage ? 'active' : null)} onClick={handleCurrentAlbumReleasesPage}>
                 {i}
             </Pagination.Item>
         );
     };
 
-    if (albumVersionsPagination.pages > 1) {
+    if (albumReleasesPagination.pages > 1) {
         return (
             <Row className='row'>
                 <Col className='col'>
                     <Pagination className='flex-wrap justify-content-center'>
-                        <Pagination.First className={albumVersionsPagination.page === 1 ? 'disabled' : ''} onClick={handleFirstAlbumVersionsPage} />
-                        <Pagination.Prev className={albumVersionsPagination.page === 1 ? 'disabled' : ''} onClick={handlePreviousAlbumVersionsPage} />
-                        {pages.map((page) => (page.props.id < albumVersionsMaxPages + 1 && page.props.id > albumVersionsMinPages) ? page : null)}
-                        <Pagination.Next className={albumVersionsPagination.page === albumVersionsPagination.pages ? 'disabled' : ''} onClick={handleNextAlbumVersionsPage} />
-                        <Pagination.Last className={albumVersionsPagination.page === albumVersionsPagination.pages ? 'disabled' : ''} onClick={handleLastAlbumVersionsPage} />
+                        <Pagination.First className={albumReleasesPagination.page === 1 ? 'disabled' : ''} onClick={handleFirstAlbumReleasesPage} />
+                        <Pagination.Prev className={albumReleasesPagination.page === 1 ? 'disabled' : ''} onClick={handlePreviousAlbumReleasesPage} />
+                        {pages.map((page) => (page.props.id < albumReleasesMaxPages + 1 && page.props.id > albumReleasesMinPages) ? page : null)}
+                        <Pagination.Next className={albumReleasesPagination.page === albumReleasesPagination.pages ? 'disabled' : ''} onClick={handleNextAlbumReleasesPage} />
+                        <Pagination.Last className={albumReleasesPagination.page === albumReleasesPagination.pages ? 'disabled' : ''} onClick={handleLastAlbumReleasesPage} />
                     </Pagination>
                 </Col>
             </Row>
