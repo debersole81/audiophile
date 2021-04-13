aa3caf79b1f02b69ce6408958fbb43e248dd50c4
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2021-04-13 16:02:48 -0400

    Destructure albumReleaseData props

--- src/components/AlbumReleaseTracks.js
+++ src/components/AlbumReleaseTracks.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import '../App.css';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Container from 'react-bootstrap/Container';
+import Table from 'react-bootstrap/Table';
 
-function AlbumReleaseTracks() {
-    return(
+function AlbumReleaseTracks(props) {
+
+    console.log('Render: AlbumReleaseTracks Component');
+    console.log(props);
+
+    /** Destructuring props */
+    const { albumReleaseData } = props.albumReleaseProps;
+
+    return (
         <h1>AlbumReleaseTracks</h1>
     );
 };
