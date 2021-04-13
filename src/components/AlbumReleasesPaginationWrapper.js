57ea1bb9cc1a552bbfd1423bc5d56e4b737c9fe5
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2021-04-12 21:05:53 -0400

    Change filename

---
+++ src/components/AlbumReleasesPaginationWrapper.js
@@ -0,0 +1,19 @@
+import React from "react";
+import AlbumVersionsPaginationDesktop from '../components/AlbumVersionsPaginationDesktop';
+import AlbumVersionsPaginationMobile from '../components/AlbumVersionsPaginationMobile';
+import { useViewPort } from '../custom-hooks/useViewPort';
+
+function AlbumReleasesPaginationWrapper(props) {
+
+    console.log('Render: AlbumVersionsPaginationWrapper Component');
+
+    /** Destructure width variable from useViewPort hook */
+    const { width } = useViewPort();
+  
+    /** Declare variable for minimum breakpoint value  */
+    const breakpoint = 381;
+
+    return (width < breakpoint ?  <AlbumVersionsPaginationMobile {...props} /> : <AlbumVersionsPaginationDesktop {...props} />);
+};
+
+export default AlbumReleasesPaginationWrapper;
