249b0835579423bda3cb33b9be36e302a995aeeb
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2021-04-13 15:22:58 -0400

    Import react bootstrap components and react icons

--- src/components/AlbumRelease.js
+++ src/components/AlbumRelease.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import '../App.css'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import Carousel from 'react-bootstrap/Carousel';
+import { FaHeart, FaRecordVinyl } from 'react-icons/fa';
 
-function AlbumRelease() {
+function AlbumRelease(props) {
+    console.log('Render: AlbumRelease Component');
 
-    return(
+    
+
+
+    return (
         <h1>AlbumRelease Component</h1>
     );
 };
