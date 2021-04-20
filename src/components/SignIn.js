43b33503163f9a0dc842dd8f62b4f1c1ff9d3607
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2021-04-20 09:58:40 -0400

    Build form groups for username and password

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,32 +1,23 @@
 import React from "react";
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
-function SignIn () {
-    return(
-        <div>
-            <h1>Login</h1>
-            <form>
-                <label> {""}
-                <input
-                    type="text"
-                    name="username"
-                    placeholder="Username"
-                />
-                </label>
-                <br />
-                <label> {""}
-                <input
-                    type="text"
-                    name="password"
-                    placeholder="Password"
-                />
-                </label>
-                <br />
-                <button>
-                    Submit
-                </button>
-                <br />
-            </form>
-        </div>
+function SignIn() {
+    return (
+        <Container>
+            <Form>
+                <h3>Sign in to AudioPhile</h3>
+                <Form.Group>
+                    <Form.Label>Username*</Form.Label>
+                    <Form.Control name='username' placeholder='Enter your username'></Form.Control>
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>Password*</Form.Label>
+                    <Form.Control name='password' type='password' placeholder='Enter your password'></Form.Control>
+                </Form.Group>
+            </Form>
+        </Container>
     );
 }
 
