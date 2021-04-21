16b08d8d0d3bcc5bcc55b27b7306ca94662a1154
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2021-04-21 17:04:55 -0400

    Import useViewPort hook and build conditional return statement that will render below 690 pixels

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,10 +1,44 @@
 import React from "react";
 import '../App.css';
+import { useViewPort } from '../custom-hooks/useViewPort';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
 
 function SignIn() {
+
+    /** Destructure width variable from useViewPort hook */
+    const { width } = useViewPort();
+
+    /** Declare variable for minimum breakpoint value */
+    const breakpoint = 690;
+
+    if (width > breakpoint) {
+        return (
+            
+            <Form className='signin-form'>
+                <h3 className='signin-header'>Sign in to AudioPhile</h3>
+                <Form.Group>
+                    <Form.Label>Username*</Form.Label>
+                    <Form.Control name='username' placeholder='Enter your username'></Form.Control>
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>Password*</Form.Label>
+                    <Form.Control name='password' type='password' placeholder='Enter your password'></Form.Control>
+                </Form.Group>
+                <Form.Row className='row signin-form-row'>
+                    <Col className='col' xs={12} sm={12} md={12} lg={12} xl={12}>
+                        <Button variant='dark' block>SIGN IN</Button>
+                    </Col>
+                    <Col className='col signin-link-col' xs={12} sm={12} md={12} lg={12} xl={12}>
+                        <p>No AudioPhile account?</p>
+                        <Button variant='link'>Sign up</Button>
+                    </Col>
+                </Form.Row>
+            </Form>
+        );
+    };
+
     return (
         <Form className='signin-form'>
             <h3 className='signin-header'>Sign in to AudioPhile</h3>
@@ -17,16 +51,15 @@ function SignIn() {
                 <Form.Control name='password' type='password' placeholder='Enter your password'></Form.Control>
             </Form.Group>
             <Form.Row className='row signin-form-row'>
-                <Col className='col' xl={12} lg={12} md={12} sm={12}>
-                    <Button variant='dark' block>SIGN IN</Button>
+                <Col className='col' xs={12} sm={12} md={12} lg={12} xl={12}>
+                    <Button variant='dark' size='sm' block>SIGN IN</Button>
                 </Col>
-                <Col className='col signin-link-col' xl={12} lg={12} md={12} sm={12}>
-                    <p>No AudioPhile account?</p>
-                    <Button variant='link'>Sign up</Button>
+                <Col className='col mt-2' xs={12} sm={12} md={12} lg={12} xl={12}>
+                    <Button variant='light' size='sm' block>SIGN UP</Button>
                 </Col>
             </Form.Row>
         </Form>
     );
-}
+};
 
 export default SignIn;
