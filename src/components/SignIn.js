181f612aa3d035a49d145e55f695e230347cc9ff
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2021-04-21 18:57:45 -0400

    Import audioPhile album logo and image component. Render image in return

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,6 +1,8 @@
 import React from "react";
 import '../App.css';
+import audioPhileAlbumLogo from '../assets/audiophile-album-logo.svg';
 import { useViewPort } from '../custom-hooks/useViewPort';
+import Image from 'react-bootstrap/Image';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
@@ -15,27 +17,29 @@ function SignIn() {
 
     if (width > breakpoint) {
         return (
-            
-            <Form className='signin-form'>
-                <h3 className='signin-header'>Sign in to AudioPhile</h3>
-                <Form.Group>
-                    <Form.Label>Username*</Form.Label>
-                    <Form.Control name='username' placeholder='Enter your username'></Form.Control>
-                </Form.Group>
-                <Form.Group>
-                    <Form.Label>Password*</Form.Label>
-                    <Form.Control name='password' type='password' placeholder='Enter your password'></Form.Control>
-                </Form.Group>
-                <Form.Row className='row signin-form-row'>
-                    <Col className='col' xs={12} sm={12} md={12} lg={12} xl={12}>
-                        <Button variant='dark' block>SIGN IN</Button>
-                    </Col>
-                    <Col className='col signin-link-col' xs={12} sm={12} md={12} lg={12} xl={12}>
-                        <p>No AudioPhile account?</p>
-                        <Button variant='link'>Sign up</Button>
-                    </Col>
-                </Form.Row>
-            </Form>
+            <React.Fragment>
+                <Form className='signin-form'>
+                <Image src={audioPhileAlbumLogo} />
+                    <h3 className='signin-header'>Sign in to AudioPhile</h3>
+                    <Form.Group>
+                        <Form.Label>Username*</Form.Label>
+                        <Form.Control name='username' placeholder='Enter your username'></Form.Control>
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Label>Password*</Form.Label>
+                        <Form.Control name='password' type='password' placeholder='Enter your password'></Form.Control>
+                    </Form.Group>
+                    <Form.Row className='row signin-form-row'>
+                        <Col className='col' xs={12} sm={12} md={12} lg={12} xl={12}>
+                            <Button variant='dark' block>SIGN IN</Button>
+                        </Col>
+                        <Col className='col signin-link-col' xs={12} sm={12} md={12} lg={12} xl={12}>
+                            <p>No AudioPhile account?</p>
+                            <Button variant='link'>Sign up</Button>
+                        </Col>
+                    </Form.Row>
+                </Form>
+            </React.Fragment>
         );
     };
 
