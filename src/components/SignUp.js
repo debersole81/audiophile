b158013b825fbe35e6a486d966d06ab52fcaffa9
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2021-04-22 11:29:01 -0400

    Import all dependencies

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,4 +1,11 @@
 import React from 'react'
+import "../App.css";
+import audioPhileAlbumLogo from '../assets/audiophile-album-logo.svg';
+import {useViewPort} from '../custom-hooks/useViewPort';
+import Image from 'react-bootstrap/Image';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
 
 function SignUp() {
 
