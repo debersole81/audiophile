0baf252cb1f97c6a7e333d00bc202213ce87c57f
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2021-04-21 20:04:16 -0400

    Add singin-form-logo class to audioPhileAlbumLogo

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -17,29 +17,27 @@ function SignIn() {
 
     if (width > breakpoint) {
         return (
-            <React.Fragment>
-                <Form className='signin-form'>
-                <Image src={audioPhileAlbumLogo} />
-                    <h3 className='signin-header'>Sign in to AudioPhile</h3>
-                    <Form.Group>
-                        <Form.Label>Username*</Form.Label>
-                        <Form.Control name='username' placeholder='Enter your username'></Form.Control>
-                    </Form.Group>
-                    <Form.Group>
-                        <Form.Label>Password*</Form.Label>
-                        <Form.Control name='password' type='password' placeholder='Enter your password'></Form.Control>
-                    </Form.Group>
-                    <Form.Row className='row signin-form-row'>
-                        <Col className='col' xs={12} sm={12} md={12} lg={12} xl={12}>
-                            <Button variant='dark' block>SIGN IN</Button>
-                        </Col>
-                        <Col className='col signin-link-col' xs={12} sm={12} md={12} lg={12} xl={12}>
-                            <p>No AudioPhile account?</p>
-                            <Button variant='link'>Sign up</Button>
-                        </Col>
-                    </Form.Row>
-                </Form>
-            </React.Fragment>
+            <Form className='signin-form'>
+                <Image src={audioPhileAlbumLogo} className='signin-form-logo' />
+                <h3 className='signin-header'>Sign in to AudioPhile</h3>
+                <Form.Group>
+                    <Form.Label>Username*</Form.Label>
+                    <Form.Control name='username' placeholder='Enter your username'></Form.Control>
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>Password*</Form.Label>
+                    <Form.Control name='password' type='password' placeholder='Enter your password'></Form.Control>
+                </Form.Group>
+                <Form.Row className='row signin-form-row'>
+                    <Col className='col' xs={12} sm={12} md={12} lg={12} xl={12}>
+                        <Button variant='dark' block>SIGN IN</Button>
+                    </Col>
+                    <Col className='col signin-link-col' xs={12} sm={12} md={12} lg={12} xl={12}>
+                        <p>No AudioPhile account?</p>
+                        <Button variant='link'>Sign up</Button>
+                    </Col>
+                </Form.Row>
+            </Form>
         );
     };
 
