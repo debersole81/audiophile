fe4a9eed968dca63640aa5dab0ac849674408db9
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2021-04-22 15:27:31 -0400

    Build functional component. Import dependecies

---
+++ src/components/ConfirmSignUp.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../App.css';
+import audioPhileLogoTextBlack from '../assets/audiophile-logo-text-black.svg';
+import { useViewPort } from '../custom-hooks/useViewPort';
+import Image from 'react-bootstrap/Image';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+
+function ConfirmSignUp() {
+
+    return(
+        <h1>This will confirm signup</h1>
+    );
+
+
+};
+
+export default ConfirmSignUp;
