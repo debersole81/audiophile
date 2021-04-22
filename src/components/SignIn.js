9909ecdb1a6fb838356b9df17013b82463df01a7
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2021-04-22 15:13:57 -0400

    Add forgot password text and link to desktop and mobile forms

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -16,7 +16,7 @@ function SignIn() {
     const breakpoint = 761;
 
     if (width > breakpoint) {
-    //Render desktop form
+        //Render desktop form
         return (
             <Form className='auth-form'>
                 <Image src={audioPhileAlbumLogo} className='auth-form-logo' />
@@ -61,6 +61,20 @@ function SignIn() {
                 <Form.Label>Password*</Form.Label>
                 <Form.Control name='password' type='password' placeholder='Enter your password'></Form.Control>
             </Form.Group>
+            {(width > 370 && width < breakpoint) ?
+                <Form.Row className='row auth-pw-form-row'>
+                    <Col className='col auth-pw-link-col text-nowrap'>
+                        <p>Forgot your password?</p>
+                        <Button variant='link'>Reset password</Button>
+                    </Col>
+                </Form.Row>
+                :
+                <Form.Row className='row auth-pw-form-row-mobile'>
+                    <Col className='col' xs={12} sm={12} md={12} lg={12} xl={12}>
+                        <Button variant='link' size='sm' block>Forgot password? Reset</Button>
+                    </Col>
+                </Form.Row>
+            }
             <Form.Row className='row auth-form-row'>
                 <Col className='col' xs={12} sm={12} md={12} lg={12} xl={12}>
                     <Button variant='dark' size='sm' block>SIGN IN</Button>
