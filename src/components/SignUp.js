2af4ffa26edccfc7f8f43ba9eac7fc0c23c1ae63
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2021-04-22 12:01:49 -0400

    Implement useViewPort hook

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -9,34 +9,43 @@ import Col from 'react-bootstrap/Col';
 
 function SignUp() {
 
-    return (
-        <Form className='signin-form'>
-            <Image src={audioPhileAlbumLogo} className='signin-form-logo' />
-            <h3 className='signin-header'>Create a new account</h3>
-            <Form.Group>
-                <Form.Label>Username*</Form.Label>
-                <Form.Control name='username' placeholder='Username' required></Form.Control>
-            </Form.Group>
-            <Form.Group>
-                <Form.Label>Password*</Form.Label>
-                <Form.Control name='password' type='password' placeholder='Password' required></Form.Control>
-            </Form.Group>
-            <Form.Group>
-                <Form.Label>Email Address*</Form.Label>
-                <Form.Control name='email' placeholder='Email' required></Form.Control>
-            </Form.Group>
-            <Form.Row className='row signin-form-row'>
-                <Col className='col' xs={12} sm={12} md={12} lg={12} xl={12}>
-                    <Button variant='dark' block>CREATE ACCOUNT</Button>
-                </Col>
-                <Col className='col signin-link-col' xs={12} sm={12} md={12} lg={12} xl={12}>
-                    <p>Have an AudioPhile account?</p>
-                    <Button variant='link'>Sign In</Button>
-                </Col>
-            </Form.Row>
-        </Form>
+    /** Destructure width variable from useViewPort hook */
+    const { width } = useViewPort();
 
-    );
+    /** Declare variable for minimum breakpoint value */
+    const breakpoint = 690;
+
+    if (width > breakpoint) {
+    //Render desktop form    
+        return (
+            <Form className='signin-form'>
+                <Image src={audioPhileAlbumLogo} className='signin-form-logo' />
+                <h3 className='signin-header'>Create a new account</h3>
+                <Form.Group>
+                    <Form.Label>Username*</Form.Label>
+                    <Form.Control name='username' placeholder='Username' required></Form.Control>
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>Password*</Form.Label>
+                    <Form.Control name='password' type='password' placeholder='Password' required></Form.Control>
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>Email Address*</Form.Label>
+                    <Form.Control name='email' placeholder='Email' required></Form.Control>
+                </Form.Group>
+                <Form.Row className='row signin-form-row'>
+                    <Col className='col' xs={12} sm={12} md={12} lg={12} xl={12}>
+                        <Button variant='dark' block>CREATE ACCOUNT</Button>
+                    </Col>
+                    <Col className='col signin-link-col' xs={12} sm={12} md={12} lg={12} xl={12}>
+                        <p>Have an AudioPhile account?</p>
+                        <Button variant='link'>Sign In</Button>
+                    </Col>
+                </Form.Row>
+            </Form>
+
+        );
+    };
 };
 
 export default SignUp;
