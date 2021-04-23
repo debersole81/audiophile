50d573c83bae1131943b8d849a506cded38e5f6c
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2021-04-23 10:29:42 -0400

    Import dependencies and build functional component

---
+++ src/components/ResetPassword.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../App.css';
+import audioPhileLogoTextBlack from '../assets/audiophile-logo-text-black.svg';
+import { useViewPort } from '../custom-hooks/useViewPort';
+import Image from 'react-bootstrap/Image';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+
+function ResetPassword() {
+
+    return(
+        <h1>Reset your password</h1>
+    );
+};
+
+export default ResetPassword;
