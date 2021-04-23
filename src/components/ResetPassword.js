818276d49390d4e165488e15d85b9b8906eb3b1c
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2021-04-23 10:41:13 -0400

    Build desktop and mobile forms

--- src/components/ResetPassword.js
+++ src/components/ResetPassword.js
@@ -9,8 +9,52 @@ import Col from 'react-bootstrap/Col';
 
 function ResetPassword() {
 
-    return(
-        <h1>Reset your password</h1>
+    /** Destructure width variable from useViewPort hook */
+    const { width } = useViewPort();
+
+    /** Declare variable for minimum breakpoint value */
+    const breakpoint = 761;
+
+    if (width > breakpoint) {
+        //Render desktop form
+        return (
+            <Form className='authaccount-form'>
+                <Image src={audioPhileLogoTextBlack} className='authaccount-logo' />
+                <h3 className='authaccount-header'>Reset your password</h3>
+                <Form.Group>
+                    <Form.Label>Username*</Form.Label>
+                    <Form.Control name='username' placeholder='Enter your username' required></Form.Control>
+                </Form.Group>
+                <Form.Row className='row authaccount-link-row'>
+                    <Col className='col' xs={12} sm={12} md={12} lg={12} xl={12}>
+                        <Button variant='dark' block>SEND CODE</Button>
+                    </Col>
+                    <Col className='col authaccount-link-col' xs={12} sm={12} md={12} lg={12} xl={12}>
+                        <Button variant='link' size='sm'>Return to sign in</Button>
+                    </Col>
+                </Form.Row>
+            </Form>
+        );
+    };
+
+    //Render mobile form
+    return (
+        <Form className='authaccount-form-mobile'>
+            <Image src={audioPhileLogoTextBlack} className='authaccount-logo-mobile' />
+            <h3 className='authaccount-header-mobile'>Reset your password</h3>
+            <Form.Group>
+                <Form.Label>Username*</Form.Label>
+                <Form.Control name='username' placeholder='Enter your username' required></Form.Control>
+            </Form.Group>
+            <Form.Row className='row authaccount-link-row'>
+                <Col className='col' xs={12} sm={12} md={12} lg={12} xl={12}>
+                    <Button variant='dark' size='sm' block>SEND CODE</Button>
+                </Col>
+                <Col className='col mt-2' xs={12} sm={12} md={12} lg={12} xl={12}>
+                    <Button variant='light' size='sm' block>RETURN TO SIGN UP</Button>
+                </Col>
+            </Form.Row>
+        </Form>
     );
 };
 
