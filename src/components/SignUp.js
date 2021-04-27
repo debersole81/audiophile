a5062430db2023acd8be2042d3729ba4ec11dacf
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2021-04-27 13:47:09 -0400

    Add form control and feedback elements to mobile form

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -90,15 +90,43 @@ function SignUp(props) {
             <h3 className='authform-header'>Create a new account</h3>
             <Form.Group>
                 <Form.Label>Username*</Form.Label>
-                <Form.Control name='username' placeholder='Username' onChange={onFormChange} required></Form.Control>
+                <Form.Control
+                    name='username'
+                    placeholder='Username'
+                    onChange={onFormChange}
+                    isInvalid={!!formErrors.username}
+                    required>
+                </Form.Control>
+                <Form.Control.Feedback className='global-form-feedback' type='inValid'>
+                    {formErrors.username}
+                </Form.Control.Feedback>
             </Form.Group>
             <Form.Group>
                 <Form.Label>Password*</Form.Label>
-                <Form.Control name='password' type='password' placeholder='Password' onChange={onFormChange} required></Form.Control>
+                <Form.Control
+                    name='password'
+                    type='password'
+                    placeholder='Password'
+                    onChange={onFormChange}
+                    isInvalid={!!formErrors.password}
+                    required>
+                </Form.Control>
+                <Form.Control.Feedback className='global-form-feedback' type='inValid'>
+                    {formErrors.password}
+                </Form.Control.Feedback>
             </Form.Group>
             <Form.Group>
                 <Form.Label>Email Address*</Form.Label>
-                <Form.Control name='email' placeholder='Email' required></Form.Control>
+                <Form.Control
+                    name='email'
+                    placeholder='Email'
+                    onChange={onFormChange}
+                    isInvalid={!!formErrors.email}
+                    required>
+                </Form.Control>
+                <Form.Control.Feedback className='global-form-feedback' type='inValid'>
+                    {formErrors.email}
+                </Form.Control.Feedback>
             </Form.Group>
             <Form.Row className='row authform-row'>
                 <Col className='col' xs={12} sm={12} md={12} lg={12} xl={12}>
