b024d1085d42e1eb1342bb6700fded9d83b9a5d1
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2021-04-27 13:45:32 -0400

    Destructure signUp callback and assign it to onClick attribute on the create account button

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -11,6 +11,8 @@ function SignUp(props) {
 
     /* #region Props destructure */
     const { onFormChange } = props;
+    const { signUp } = props;
+    const { formErrors } = props;
     /* #endregion Props destructure */
 
     /* #region Custom hooks */
@@ -30,19 +32,47 @@ function SignUp(props) {
                 <h3 className='authform-header'>Create a new account</h3>
                 <Form.Group>
                     <Form.Label>Username*</Form.Label>
-                    <Form.Control name='username' placeholder='Username' onChange={onFormChange} required></Form.Control>
+                    <Form.Control
+                        name='username'
+                        placeholder='Username'
+                        onChange={onFormChange}
+                        isInvalid={!!formErrors.username}
+                        required>
+                    </Form.Control>
+                    <Form.Control.Feedback className='global-form-feedback' type='inValid'>
+                        {formErrors.username}
+                    </Form.Control.Feedback>
                 </Form.Group>
                 <Form.Group>
                     <Form.Label>Password*</Form.Label>
-                    <Form.Control name='password' type='password' placeholder='Password' onChange={onFormChange} required></Form.Control>
+                    <Form.Control
+                        name='password'
+                        type='password'
+                        placeholder='Password'
+                        onChange={onFormChange}
+                        isInvalid={!!formErrors.password}
+                        required>
+                    </Form.Control>
+                    <Form.Control.Feedback className='global-form-feedback' type='inValid'>
+                        {formErrors.password}
+                    </Form.Control.Feedback>
                 </Form.Group>
                 <Form.Group>
                     <Form.Label>Email Address*</Form.Label>
-                    <Form.Control name='email' placeholder='Email' onChange={onFormChange} required></Form.Control>
+                    <Form.Control
+                        name='email'
+                        placeholder='Email'
+                        onChange={onFormChange}
+                        isInvalid={!!formErrors.email}
+                        required>
+                    </Form.Control>
+                    <Form.Control.Feedback className='global-form-feedback' type='inValid'>
+                        {formErrors.email}
+                    </Form.Control.Feedback>
                 </Form.Group>
                 <Form.Row className='row authform-row'>
                     <Col className='col' xs={12} sm={12} md={12} lg={12} xl={12}>
-                        <Button variant='dark' block>CREATE ACCOUNT</Button>
+                        <Button variant='dark' onClick={signUp} block>CREATE ACCOUNT</Button>
                     </Col>
                     <Col className='col authform-link-col' xs={12} sm={12} md={12} lg={12} xl={12}>
                         <p>Have an account?</p>
