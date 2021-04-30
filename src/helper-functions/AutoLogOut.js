15b1a7f8707bf9cfe2b8ef7759234e9ff8014d80
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2021-04-30 13:14:09 -0400

    Build helper function to automatically logout authenticated user

---
+++ src/helper-functions/AutoLogOut.js
@@ -0,0 +1,43 @@
+/** Auto Logout Authenticated User */
+
+function autoLogOut() {
+    /** Construct events array */
+    const events = [
+        'load',
+        'mousemove',
+        'mousedown',
+        'click',
+        'scroll',
+        'keypress'
+    ];
+
+    let warnTimer;
+    let logoutTimer;
+
+    /** Warn authenticated user of auto logout */
+    // const warn = () => alert('You will be logged out due to inactivity. Click ok to remain logged in');
+    /** Clear local storage when user is logged out */
+    const logout = () => window.localStorage.clear();
+
+    /** Declare timer method */
+    const setTimer = () => {
+        warnTimer = setTimeout(warn, 1000);
+        logoutTimer = setTimeout(logout, 5000);
+    };
+
+    /** Declare clear timer method */
+    const clearTimer = () => {
+        if (warnTimer) clearTimeout(warnTimer);
+        if (logoutTimer) clearTimeout(logoutTimer);
+    };
+
+    /** Declare reset timer method that will fire on each event */
+    const resetTimer = () => {
+        clearTimer();
+        setTimer();
+    };
+
+    return (events.forEach((event) => window.addEventListener(event, resetTimer)))
+};
+
+export { autoLogOut };
