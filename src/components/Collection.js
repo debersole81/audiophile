c6c08a86a640a0fd098f71bd59a20e7d6317a97a
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2021-05-10 13:38:15 -0400

    Destructure userCollection albums and release props

--- src/components/Collection.js
+++ src/components/Collection.js
@@ -4,9 +4,14 @@ import React from "react";
  * When an album in the collection is clicked, use the handleAlbumClick callback in ProtectedComponents to render the album
  */
 
-function Collection () {
+function Collection(props) {
+    
+    /* #region Destructure Props */
+    const { userCollectionAlbums, userCollectionReleases } = props.collectionProps;
+    console.log(userCollectionAlbums, userCollectionReleases);
+    /* #endregion Destructure Props */
 
-    return(
+    return (
         <h1>Collection</h1>
     );
 };
