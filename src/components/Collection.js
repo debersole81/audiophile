74c5eb27e921f96703fabaf09707756f54ead0ff
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2021-05-11 14:20:55 -0400

    Build conditional return statement to display a link to the search component if user album collection is empty

--- src/components/Collection.js
+++ src/components/Collection.js
@@ -56,70 +56,89 @@ function Collection(props) {
     /* #endregion Join and Sort User Albums and Releases */
 
 
-    if (Array.isArray(sortedUserCollection) && sortedUserCollection.length) {
-        return (
-            <React.Fragment>
-                <Jumbotron fluid className='user-library-jumbotron'>
-                    <Row className='row'>
-                        <Col className='col'>
-                            <h1>Album Collection</h1>
-                        </Col>
-                    </Row>
-                    <Row className='row'>
-                        <Col className='col'>
-                            <p className='lead text-muted'>You've got great taste! Browse your collection of albums here.</p>
-                        </Col>
-                    </Row>
-                </Jumbotron>
-                <Container>
-                    <Row>
-                        {sortedUserCollection.map((albums) =>
-                            <Col key={albums.albumId} className='col user-libary-col'>
-                                <Card className='user-library-card'>
-                                    <div className='overflow'>
-                                        <Card.Img className='user-library-card-img overflow' variant='top' src={albums.albumImage} alt='Album Cover Art' />
-                                    </div>
-                                    <Card.Body>
-                                        <Card.Title className='text-truncate'>{albums.albumTitle}</Card.Title>
-                                        <Card.Subtitle className='mb-4 text-truncate text-muted'>{albums.artistName}</Card.Subtitle>
-                                        <Row className='row'>
-                                            {(albums.albumId === albums.mainReleaseId) ?
-                                                <Col className='col' xs={4} s={4} md={4} lg={4} xl={4}>
-                                                    <Image className='user-library-album-master-logo' src={masterReleaseLogo} alt='Album Master Release Logo' />
-                                                </Col> :
-                                                <Col className='col' xs={4} s={4} md={4} lg={4} xl={4}>
-                                                </Col>
-                                            }
-                                            {(albums.albumId === albums.mainReleaseId) ?
-                                                <Col className='col' xs={8} s={8} md={8} lg={8} xl={8}>
-                                                    <ButtonGroup className='user-library-btn-group'>
-                                                        <Button id={albums.id} variant='outline-dark' size='sm' onClick={deleteAlbumFromCollection} block>Remove</Button>
-                                                    </ButtonGroup>
-                                                    <ButtonGroup className='user-library-btn-group'>
-                                                        <Button id={albums.masterId} className='pl-3 pr-3' variant='outline-dark' size='sm' onClick={handleAlbumClick} block>View</Button>
-                                                    </ButtonGroup>
-                                                </Col> :
-                                                <Col className='col' xs={8} s={8} md={8} lg={8} xl={8}>
-                                                    <ButtonGroup className='user-library-btn-group'>
-                                                        <Button id={albums.id} variant='outline-dark' size='sm' onClick={deleteReleaseFromColection} block>Remove</Button>
-                                                    </ButtonGroup>
-                                                    <ButtonGroup className='user-library-btn-group'>
-                                                        <Button id={albums.albumId} className='pl-3 pr-3' variant='outline-dark' size='sm' onClick={handleViewAlbumRelease} block>View</Button>
-                                                    </ButtonGroup>
-                                                </Col>
-                                            }
-                                        </Row>
-                                    </Card.Body>
-                                </Card>
-                            </Col>
-                        )}
-                    </Row>
-                </Container>
-            </React.Fragment>
-        );
-    }
+    // if (Array.isArray(sortedUserCollection) && sortedUserCollection.length) {
+    //     return (
+    //         <React.Fragment>
+    //             <Jumbotron fluid className='user-library-jumbotron'>
+    //                 <Row className='row'>
+    //                     <Col className='col'>
+    //                         <h1>Album Collection</h1>
+    //                     </Col>
+    //                 </Row>
+    //                 <Row className='row'>
+    //                     <Col className='col'>
+    //                         <p className='lead text-muted'>You've got great taste! Browse your collection of albums here.</p>
+    //                     </Col>
+    //                 </Row>
+    //             </Jumbotron>
+    //             <Container>
+    //                 <Row>
+    //                     {sortedUserCollection.map((albums) =>
+    //                         <Col key={albums.albumId} className='col user-libary-col'>
+    //                             <Card className='user-library-card'>
+    //                                 <div className='overflow'>
+    //                                     <Card.Img className='user-library-card-img overflow' variant='top' src={albums.albumImage} alt='Album Cover Art' />
+    //                                 </div>
+    //                                 <Card.Body>
+    //                                     <Card.Title className='text-truncate'>{albums.albumTitle}</Card.Title>
+    //                                     <Card.Subtitle className='mb-4 text-truncate text-muted'>{albums.artistName}</Card.Subtitle>
+    //                                     <Row className='row'>
+    //                                         {(albums.albumId === albums.mainReleaseId) ?
+    //                                             <Col className='col' xs={4} s={4} md={4} lg={4} xl={4}>
+    //                                                 <Image className='user-library-album-master-logo' src={masterReleaseLogo} alt='Album Master Release Logo' />
+    //                                             </Col> :
+    //                                             <Col className='col' xs={4} s={4} md={4} lg={4} xl={4}>
+    //                                             </Col>
+    //                                         }
+    //                                         {(albums.albumId === albums.mainReleaseId) ?
+    //                                             <Col className='col' xs={8} s={8} md={8} lg={8} xl={8}>
+    //                                                 <ButtonGroup className='user-library-btn-group'>
+    //                                                     <Button id={albums.id} variant='outline-dark' size='sm' onClick={deleteAlbumFromCollection} block>Remove</Button>
+    //                                                 </ButtonGroup>
+    //                                                 <ButtonGroup className='user-library-btn-group'>
+    //                                                     <Button id={albums.masterId} className='pl-3 pr-3' variant='outline-dark' size='sm' onClick={handleAlbumClick} block>View</Button>
+    //                                                 </ButtonGroup>
+    //                                             </Col> :
+    //                                             <Col className='col' xs={8} s={8} md={8} lg={8} xl={8}>
+    //                                                 <ButtonGroup className='user-library-btn-group'>
+    //                                                     <Button id={albums.id} variant='outline-dark' size='sm' onClick={deleteReleaseFromColection} block>Remove</Button>
+    //                                                 </ButtonGroup>
+    //                                                 <ButtonGroup className='user-library-btn-group'>
+    //                                                     <Button id={albums.albumId} className='pl-3 pr-3' variant='outline-dark' size='sm' onClick={handleViewAlbumRelease} block>View</Button>
+    //                                                 </ButtonGroup>
+    //                                             </Col>
+    //                                         }
+    //                                     </Row>
+    //                                 </Card.Body>
+    //                             </Card>
+    //                         </Col>
+    //                     )}
+    //                 </Row>
+    //             </Container>
+    //         </React.Fragment>
+    //     );
+    // }
 
-    return (null);
+    return (
+        <React.Fragment>
+            <Jumbotron className='user-library-jumbotron'>
+                <Row className='row'>
+                    <Col className='col'>
+                        <h1>Album Collection</h1>
+                    </Col>
+                </Row>
+            </Jumbotron>
+            <Container className='user-library-empty'>
+                <Row className='row'>
+                    <Col className='col'>
+                        <h1>Search for albums to add to your collection.</h1>
+                        <Button variant='outline-dark' className='col-6 mx-auto' size='sm' block>Search</Button>
+                    </Col>
+                </Row>
+            </Container>
+        </React.Fragment>
+    );
+
     //Search for albums text + button
 };
 
