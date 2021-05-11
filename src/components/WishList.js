ffcf77a4120c90463e75d6fadc56090b84495678
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2021-05-11 14:55:11 -0400

    Destructure wishListProps

--- src/components/WishList.js
+++ src/components/WishList.js
@@ -1,8 +1,39 @@
 import React from "react";
+import { useHistory } from 'react-router-dom';
+import '../App.css';
+import masterReleaseLogo from '../assets/master-release-logo.svg';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import Image from 'react-bootstrap/Image';
+import Jumbotron from "react-bootstrap/Jumbotron";
 
-function WishList () {
+function WishList(props) {
 
-    return(
+    /* #region Global Variables */
+    /** useHistory hook variable. */
+    const history = useHistory();
+    /* #endregion Global Variables */
+
+    /* #region Destructure Props */
+    const {
+        userWishListAlbums,
+        userWishListReleases,
+        handleAlbumClick,
+        handleViewAlbumRelease,
+        deleteAlbumFromWishList,
+        deleteReleaseFromWishList
+    } = props.wishListProps;
+    /* #endregion Destructure Props */
+
+
+
+
+
+    return (
         <h1>Wish List</h1>
     );
 };
