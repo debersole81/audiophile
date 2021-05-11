1c1444551b0f10e19547867fb12b94b1b6d6f96f
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2021-05-11 14:59:12 -0400

    Build functionality to join and sort user wishlist albums and releases

--- src/components/WishList.js
+++ src/components/WishList.js
@@ -29,7 +29,33 @@ function WishList(props) {
     } = props.wishListProps;
     /* #endregion Destructure Props */
 
+    /* #region Join and Sort User Albums and Releases */
+    /** Join user albums and releases, assign output to userCollection */
+    const userWishList = userWishListAlbums.concat(userWishListReleases);
 
+    /**Sort userWishList by artist name and then by album name */
+    //Define compare function to pass to array.sort method
+    function compare(a, b) {
+        //Declare variables for artistName fields
+        const firstArtistName = a.artistName.toUpperCase();
+        const secondArtistName = b.artistName.toUpperCase();
+        //Declare variables for albumTitle fields
+        const firstAlbumTitle = a.albumTitle.toUpperCase();
+        const secondAlbumTitle = b.albumTitle.toUpperCase();
+
+        // If artistNames are equal, sort by albumTitle
+        if (firstArtistName === secondArtistName) {
+            return (firstAlbumTitle > secondAlbumTitle) ? 1 : (firstAlbumTitle < secondAlbumTitle) ? -1 : 0;
+            //If artistNames are not equal, sort first by artistName and then repeat loop to sort albumTitles
+        } else {
+            return (firstArtistName < secondArtistName) ? -1 : 1;
+        }
+    };
+    //Declare a new variable to hold the sorted userCollection array
+    const sortedUserWishList = userWishList.sort(compare);
+    /* #endregion Join and Sort User Albums and Releases */
+    console.log(sortedUserWishList);
+    
 
 
 
