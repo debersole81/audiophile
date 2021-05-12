5adace6ca95b09cdc18b1f553197e62530f8a79b
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2021-05-12 16:33:58 -0400

    Import images and build image elements

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,17 +1,37 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../App.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
+import collection from '../assets/collection.jpg';
+import wishList from '../assets/wish-list.jpg';
+import search from '../assets/search.jpg';
+import randomizer from '../assets/randomizer.jpg';
 
-function Dashboard(props) {
+function Dashboard() {
 
     return (
-        <div>
-            <Link to='/collection'> Collection </Link> <br />
-            <Link to='/wishlist'> Wish List </Link> <br />
-            <Link to='/randomizer'> Randomizer </Link> <br />
-            <Link to='/search'> Search </Link>
-        </div>
+        <Container>
+            <Row className='row'>
+                <Col className='dashboard-col'>
+                    <Image src={collection} className='img-fluid dashboard-image' />
+                </Col>
+                <Col className='dashboard-col'>
+                    <Image src={wishList} className='img-fluid dashboard-image' />
+                </Col>
+            </Row>
+            <Row className='row'>
+                <Col className='dashboard-col'>
+                    <Image src={search} className='img-fluid dashboard-image' />
+                </Col>
+                <Col className='dashboard-col'>
+                    <Image src={randomizer} className='img-fluid dashboard-image' />
+                </Col>
+            </Row>
+        </Container>
     );
-
 };
 
 export default Dashboard;
