28481a5b14432534392410da081b5f783bbc6da8
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2021-06-24 17:44:13 -0400

    Fix conditional error in return statement

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -12,36 +12,39 @@ function SearchResults(props) {
     const { searchData, albumClick, searchDataLoading } = props.searchResultsProps;
     /* #endregion Props destructure */
 
-    return (
-        <React.Fragment>
-            {
-                (Array.isArray(searchData) && searchData.length && !searchDataLoading) &&
-                <Container>
-                    <Row>
-                        {searchData.map((results) =>
-                            <Col key={results.id} className='col search-results-col' md={6} lg={4}>
-                                <Card className='search-results-card'>
-                                    <div className='overflow'>
-                                        <Card.Img className='search-results-card-img overflow' variant='top' src={results.cover_image} alt='Album Cover Art' />
-                                    </div>
-                                    <Card.Body>
-                                        <Card.Title className='text-truncate'>{results.title.split(' - ')[1]}</Card.Title>
-                                        <Card.Subtitle className='mb-2 text-truncate text-muted'>{results.title.split(' - ')[0]}</Card.Subtitle>
-                                        <Card.Text className='text-truncate'>
-                                            {results.label[0]}<br />
-                                            {results.year}<br />
-                                        </Card.Text>
-                                        <Button variant='outline-dark' size='sm' id={results.master_id} onClick={albumClick} block>View</Button>
-                                    </Card.Body>
-                                </Card>
-                            </Col>
-                        )}
-                    </Row>
-                </Container>
-            }
-            {null}
-        </React.Fragment>
-    );
+    if (searchDataLoading) {
+        return (<h1>Loading...</h1>)
+    }
+
+    if (Array.isArray(searchData) && searchData.length && !searchDataLoading) {
+        return (
+            <Container>
+                <Row>
+                    {searchData.map((results) =>
+                        <Col key={results.id} className='col search-results-col' md={6} lg={4}>
+                            <Card className='search-results-card'>
+                                <div className='overflow'>
+                                    <Card.Img className='search-results-card-img overflow' variant='top' src={results.cover_image} alt='Album Cover Art' />
+                                </div>
+                                <Card.Body>
+                                    <Card.Title className='text-truncate'>{results.title.split(' - ')[1]}</Card.Title>
+                                    <Card.Subtitle className='mb-2 text-truncate text-muted'>{results.title.split(' - ')[0]}</Card.Subtitle>
+                                    <Card.Text className='text-truncate'>
+                                        {results.label[0]}<br />
+                                        {results.year}<br />
+                                    </Card.Text>
+                                    <Button variant='outline-dark' size='sm' id={results.master_id} onClick={albumClick} block>View</Button>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    )}
+                </Row>
+            </Container>
+        );
+
+    }
+
+    return (null);
 };
 
 export default SearchResults;
