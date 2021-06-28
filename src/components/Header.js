a1130d7e20d8d621fc7ce13048ffb652b49e7f9e
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2021-06-28 11:57:30 -0400

    Format code, fix href attribute on Navbar.Brand element

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,70 +1,85 @@
 import React from "react";
-import '../App.css';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import Button from 'react-bootstrap/Button';
-import { LinkContainer } from 'react-router-bootstrap';
-import audioPhileLogoText from '../assets/audiophile-logo-text-white.svg';
-import { useViewPort } from '../custom-hooks/useViewPort';
+import "../App.css";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Button from "react-bootstrap/Button";
+import { LinkContainer } from "react-router-bootstrap";
+import audioPhileLogoText from "../assets/audiophile-logo-text-white.svg";
+import { useViewPort } from "../custom-hooks/useViewPort";
 
 function Header(props) {
+  /* #region Destructure Props */
+  const { logOut } = props;
+  /* #endregion Destructure Props */
 
-    /* #region Destructure Props */
-    const { logOut } = props;
-    /* #endregion Destructure Props */
-
-    /* #region Set Mobile Breakpoint with Custom Hook
+  /* #region Set Mobile Breakpoint with Custom Hook
     /** Destructure width variable from useViewPort hook */
-    const { width } = useViewPort();
+  const { width } = useViewPort();
 
-    /** Declare variable for minimum breakpoint value */
-    const breakpoint = 345;
-    /* #endregion Set Mobile Breakpoint with Custom Hook */
+  /** Declare variable for minimum breakpoint value */
+  const breakpoint = 345;
+  /* #endregion Set Mobile Breakpoint with Custom Hook */
 
-    return (
-        <Navbar collapseOnSelect sticky='top' bg='dark' variant='dark' className='p-4 header' expand='lg'>
-            {(width > breakpoint) ?
-                <Navbar.Brand href='/'>
-                    <img
-                        src={audioPhileLogoText}
-                        height={30}
-                        width={225}
-                        className='d-inline-block align-top'
-                        alt='AudioPhile logo'
-                    />
-                </Navbar.Brand> :
-                <Navbar.Brand href='#home'>
-                    <img
-                        src={audioPhileLogoText}
-                        height={30}
-                        width={160}
-                        className='d-inline-block align-top'
-                        alt='AudioPhile logo'
-                    />
-                </Navbar.Brand>}
-            <Navbar.Toggle aria-controls='basic-navbar-nav' />
-            <Navbar.Collapse id='basic-navbar-nav'>
-                <Nav className='ml-auto'>
-                    <LinkContainer exact to='/'>
-                        <Nav.Link>Dashboard</Nav.Link>
-                    </LinkContainer>
-                    <LinkContainer exact to='/collection'>
-                        <Nav.Link>Collection</Nav.Link>
-                    </LinkContainer>
-                    <LinkContainer exact to='/wishlist'>
-                        <Nav.Link>Wish List</Nav.Link>
-                    </LinkContainer>
-                    {/* <LinkContainer exact to="/randomizer">
+  return (
+    <Navbar
+      collapseOnSelect
+      sticky="top"
+      bg="dark"
+      variant="dark"
+      className="p-4 header"
+      expand="lg"
+    >
+      {width > breakpoint ? (
+        <Navbar.Brand href="/">
+          <img
+            src={audioPhileLogoText}
+            height={30}
+            width={225}
+            className="d-inline-block align-top"
+            alt="AudioPhile logo"
+          />
+        </Navbar.Brand>
+      ) : (
+        <Navbar.Brand href="/">
+          <img
+            src={audioPhileLogoText}
+            height={30}
+            width={160}
+            className="d-inline-block align-top"
+            alt="AudioPhile logo"
+          />
+        </Navbar.Brand>
+      )}
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ml-auto">
+          <LinkContainer exact to="/">
+            <Nav.Link>Dashboard</Nav.Link>
+          </LinkContainer>
+          <LinkContainer exact to="/collection">
+            <Nav.Link>Collection</Nav.Link>
+          </LinkContainer>
+          <LinkContainer exact to="/wishlist">
+            <Nav.Link>Wish List</Nav.Link>
+          </LinkContainer>
+          {/* <LinkContainer exact to="/randomizer">
                         <Nav.Link>Randomizer</Nav.Link>
                     </LinkContainer> */}
-                    <LinkContainer className='mr-3' exact to="/search">
-                        <Nav.Link>Search</Nav.Link>
-                    </LinkContainer>
-                    <Button variant='outline-light' size='sm' className='header-btn' onClick={logOut}>Logout</Button>
-                </Nav>
-            </Navbar.Collapse>
-        </Navbar>
-    );
-};
+          <LinkContainer className="mr-3" exact to="/search">
+            <Nav.Link>Search</Nav.Link>
+          </LinkContainer>
+          <Button
+            variant="outline-light"
+            size="sm"
+            className="header-btn"
+            onClick={logOut}
+          >
+            Logout
+          </Button>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+}
 
 export default Header;
