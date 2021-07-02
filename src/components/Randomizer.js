0ed4fc3446135c88f346215ede2a3c6acc4f602f
Doug Ebersole <71712303+debersole81@users.noreply.github.com>
2021-07-02 16:15:01 -0400

    Add notes

--- src/components/Randomizer.js
+++ src/components/Randomizer.js
@@ -4,18 +4,19 @@ import React from "react";
  * Display the image of the AudioPhile LP logo
  * Logo will spin when the randomizer button is clicked
  * While spinning, an album is selected from the user's collection (Amplify or Dynamo DB)
- * After spinning, 
+ * After spinning,
  * The album image will replace the logo
  * A 'view album details' button will render. onClick will call handleAlbumClick callback in ProtectedComponents)
  * The randomize button will remain but beneath the view album details button
- * If randomize is clicked again, the AudioPhile logo reappears, begins to spin while a new album is selected and the whole process repeats. 
-*/
+ * If randomize is clicked again, the AudioPhile logo reappears, begins to spin while a new album is selected and the whole process repeats.
+ */
 
-function Randomizer () {
+function Randomizer(props) {
+  //Concat user albums and releases, assign to a variable
 
-    return(
-        <h1>Randomizer</h1>
-    );
-};
+  //Should I sort the albums before randomizing?
+
+  return <h1>Randomizer</h1>;
+}
 
 export default Randomizer;
